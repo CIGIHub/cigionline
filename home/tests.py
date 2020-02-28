e211f14c5d3627ca737a50bfc61b5e5909679ec8
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-02-28 13:53:06 -0500

    Update home page model to be a singleton

---
+++ home/tests.py
@@ -0,0 +1,20 @@
+from wagtail.core.models import Page
+from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils.form_data import nested_form_data
+
+from .models import HomePage
+
+class HomePageTests(WagtailPageTests):
+    def test_cannot_create_homepage(self):
+        """
+        Test that we can't create a second home page. Wagtail will create a home
+        page object on initialization so we don't need to create one first.
+        """
+        root_page = Page.objects.get(pk=1)
+        try:
+            self.assertCanCreate(root_page, HomePage, nested_form_data({
+                'title': 'Home',
+            }))
+            self.fail('Expected to error')
+        except AssertionError:
+            pass
