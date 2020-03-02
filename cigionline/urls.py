4b009df7751cf9701ee6c0dee7657bc3d2e2c8c9
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-03-02 11:30:36 -0500

    hound fix

--- cigionline/urls.py
+++ cigionline/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG: # pragma: no cover
+if settings.DEBUG:  # pragma: no cover
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
