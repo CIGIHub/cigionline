04097c3c15fdaa5ca4c609b2fc7ae2b6924fa577
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-03-10 20:47:40 -0400

    Add dj-database-url

--- cigionline/settings/production.py
+++ cigionline/settings/production.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from .base import *
 
 DEBUG = False
@@ -6,3 +7,5 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
