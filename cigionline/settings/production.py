1347889dce25640aa8f3fb4600aa4fefc707d1b6
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-03-11 14:10:20 -0400

    logging

--- cigionline/settings/production.py
+++ cigionline/settings/production.py
@@ -1,6 +1,9 @@
 import dj_database_url
+import logging
 import os
 from .base import *
+
+logger = logging.getLogger(__name__)
 
 DEBUG = False
 
@@ -16,6 +19,9 @@ if 'ALLOWED_HOSTS' in os.environ:
     ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
 if 'DATABASE_URL' in os.environ:
+    logger.info('Setting DATABASE_URL:')
+    logger.info(os.environ['DATABASE_URL'])
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
+    logger.info(DATABASES)
