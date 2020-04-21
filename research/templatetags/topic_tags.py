02eae3677b7ab114ea37973869ae344fdbb70e6a
johnbxu <johnbxu@gmail.com>
2020-04-21 11:17:17 -0400

    clean up

--- research/templatetags/topic_tags.py
+++ research/templatetags/topic_tags.py
@@ -1,10 +1,10 @@
-from django import template
-register = template.Library()
-
 from ..models import TopicPage
+from django import template
+
+
+register = template.Library()
 
 @register.inclusion_tag('research/all_topics.html')
 def all_topics():
-  all_topics = TopicPage.objects.all()
-  print(all_topics)
-  return {'all_topics' : all_topics}
+    all_topics = TopicPage.objects.all()
+    return {'all_topics':all_topics}
