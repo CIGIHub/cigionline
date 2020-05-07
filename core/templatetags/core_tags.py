fe8e4b5f653f88778fffe48c6ede8c1c6ba7f2b7
johnbxu <johnbxu@gmail.com>
2020-05-07 15:21:01 -0400

    Added external publications to template

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -3,5 +3,10 @@ register = template.Library()
 
 
 @register.filter
+def trim(value):
+    return value.strip()
+    
+
+@register.filter
 def no_protocol(value):
     return value.replace('http://', '').replace('https://', '')
