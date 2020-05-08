ff1122aca2def1b2be6b34cdfb55e5349b7168f8
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-05-08 15:48:13 -0400

    add node id to personpage

--- people/models.py
+++ people/models.py
@@ -113,7 +113,6 @@ class PersonPage(Page):
         THESIS = 'Thesis'
         WEB_PAGE = 'Web Page'
 
-
     address_city = models.CharField(blank=True, max_length=255)
     address_country = models.CharField(blank=True, max_length=255)
     address_line1 = models.CharField(blank=True, max_length=255)
@@ -195,6 +194,9 @@ class PersonPage(Page):
     topics = ParentalManyToManyField('research.TopicPage', blank=True)
     twitter_username = models.CharField(blank=True, max_length=255)
     website = models.URLField(blank=True)
+
+    # Reference field for the Drupal-Wagtail migrator. Can be removed after.
+    drupal_node_id = models.IntegerField(null=True)
 
     content_panels = Page.content_panels + [
         MultiFieldPanel(
