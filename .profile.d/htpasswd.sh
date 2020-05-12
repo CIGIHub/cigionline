adc175a025bf5052c5e6a41f0e528edb00344f02
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-05-12 10:08:19 -0400

    fix syntax

--- .profile.d/htpasswd.sh
+++ .profile.d/htpasswd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-if [ -n "${BASIC_AUTH_USER}" ] && [ -n "${BASIC_AUTH_PASSWORD}" ] then
+if [[ -n "${BASIC_AUTH_USER}" && -n "${BASIC_AUTH_PASSWORD}" ]]; then
   echo -e "${BASIC_AUTH_USER}:$(perl -le 'print crypt($ENV{"BASIC_AUTH_PASSWORD"}, rand(0xffffffff));')" > "${HOME}/config/.htpasswd"
 fi
