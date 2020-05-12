327ce21aac7b35ecd3dfa1b09e1bc96eedaa1bc2
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-05-12 09:37:23 -0400

    add bash condition

--- .profile.d/htpasswd.sh
+++ .profile.d/htpasswd.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-
-echo -e "${BASIC_AUTH_USER}:$(perl -le 'print crypt($ENV{"BASIC_AUTH_PASSWORD"}, rand(0xffffffff));')" > "${HOME}/config/.htpasswd"
+if [ -n "${BASIC_AUTH_USER}" ] && [ -n "${BASIC_AUTH_PASSWORD}" ] then
+  echo -e "${BASIC_AUTH_USER}:$(perl -le 'print crypt($ENV{"BASIC_AUTH_PASSWORD"}, rand(0xffffffff));')" > "${HOME}/config/.htpasswd"
+fi
