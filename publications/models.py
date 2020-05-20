9e8d37a1ae1a82a60b123fc51e7c7877e3eb07bb
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-05-19 22:04:51 -0400

    book info fields

--- publications/models.py
+++ publications/models.py
@@ -2,8 +2,9 @@ from core.models import BasicPageAbstract, ShareablePageAbstract
 from django.db import models
 from modelcluster.fields import ParentalManyToManyField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
-from wagtail.core.blocks import CharBlock, PageChooserBlock
-from wagtail.core.fields import StreamField
+from wagtail.core.blocks import CharBlock, PageChooserBlock, StructBlock, URLBlock
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.documents.edit_handlers import DocumentChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
@@ -22,6 +23,11 @@ class PublicationListPage(BasicPageAbstract):
 class PublicationPage(BasicPageAbstract, ShareablePageAbstract):
     """View publication page"""
 
+    class BookFormats(models.TextChoices):
+        HARDCOVER = ('HC', 'Hardcover')
+        PAPERBACK = ('PB', 'Paperback')
+        TRADE_PB = ('TP', 'Trade PB')
+
     authors = StreamField(
         [
             ('author', PageChooserBlock(required=True, page_type='people.PersonPage')),
@@ -29,11 +35,43 @@ class PublicationPage(BasicPageAbstract, ShareablePageAbstract):
         ],
         blank=True,
     )
+    book_excerpt = RichTextField(blank=True, verbose_name='Excerpt')
+    book_excerpt_download = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name='Excerpt Download',
+    )
+    book_format = models.CharField(
+        blank=True,
+        null=True,
+        max_length=2,
+        choices=BookFormats.choices,
+        verbose_name='Format',
+        help_text='Select the formation of this book/publication.',
+    )
+    book_pages = models.IntegerField(
+        blank=True,
+        null=True,
+        verbose_name='Pages',
+        help_text='Enter the number of pages in the book.',
+    )
+    book_publisher = models.CharField(blank=True, max_length=255)
+    book_publisher_url = models.URLField(blank=True)
+    book_purchase_links = StreamField([
+        ('purchase_link', StructBlock([
+            ('link_text', CharBlock(required=True)),
+            ('url', URLBlock(required=True)),
+        ]))
+    ], blank=True)
     editors = StreamField(
         [
             ('editor', PageChooserBlock(required=True, page_type='people.PersonPage')),
             ('external_editor', CharBlock(required=True)),
-        ]
+        ],
+        blank=True,
     )
     image_cover = models.ForeignKey(
         'wagtailimages.Image',
@@ -43,6 +81,24 @@ class PublicationPage(BasicPageAbstract, ShareablePageAbstract):
         related_name='+',
         verbose_name='Cover image',
         help_text='An image of the cover of the publication.',
+    )
+    isbn = models.CharField(
+        blank=True,
+        max_length=32,
+        verbose_name='ISBN',
+        help_text='Enter the print ISBN for this book.',
+    )
+    isbn_ebook = models.CharField(
+        blank=True,
+        max_length=32,
+        verbose_name='eBook ISBN',
+        help_text='Enter the ISBN for the eBook version of this publication.',
+    )
+    isbn_hardcover = models.CharField(
+        blank=True,
+        max_length=32,
+        verbose_name='Hardcover ISBN',
+        help_text='Enter the ISBN for the hardcover version of this publication.',
     )
     topics = ParentalManyToManyField('research.TopicPage', blank=True)
 
@@ -64,6 +120,22 @@ class PublicationPage(BasicPageAbstract, ShareablePageAbstract):
                 StreamFieldPanel('editors'),
             ],
             heading='Editors',
+            classname='collapsible collapsed',
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('book_publisher'),
+                FieldPanel('book_publisher_url'),
+                FieldPanel('book_format'),
+                FieldPanel('isbn'),
+                FieldPanel('isbn_hardcover'),
+                FieldPanel('isbn_ebook'),
+                FieldPanel('book_pages'),
+                StreamFieldPanel('book_purchase_links'),
+                DocumentChooserPanel('book_excerpt_download'),
+                FieldPanel('book_excerpt'),
+            ],
+            heading='Book Info',
             classname='collapsible collapsed',
         ),
         MultiFieldPanel(
