d2d5ff4846d3149a75a4c1bd1a2a804789f0053a
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-09-01 21:22:23 -0400

    article landing page

--- articles/models.py
+++ articles/models.py
@@ -10,10 +10,20 @@ from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
-class ArticleListPage(Page):
+class ArticleLandingPage(Page):
     max_count = 1
     parent_page_types = ['core.HomePage']
     subpage_types = []
+    templates = 'articles/article_landing_page.html'
+
+    class Meta:
+        verbose_name = 'Article Landing Page'
+
+
+class ArticleListPage(Page):
+    max_count = 1
+    parent_page_types = ['core.HomePage']
+    subpage_types = ['articles.ArticlePage']
     templates = 'articles/article_list_page.html'
 
     class Meta:
