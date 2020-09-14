49589eb9ef23768dc3d3fb1683a19527006f1be4
johnbxu <johnbxu@gmail.com>
2020-09-14 14:39:15 -0400

    Added normalize to staticfiles_dir

--- cigionline/settings/base.py
+++ cigionline/settings/base.py
@@ -161,6 +161,7 @@ STATICFILES_FINDERS = [
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
     'node_modules/@fortawesome',
+    'node_modules/normalize.css',
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
