928be2d5e7e121800bcb2dbaf5b1e437d1a873ec
franciscaluz <vfranciscaluz@gmail.com>
2020-10-01 09:43:34 -0400

    CIGI-195 Insert js (not compiling js)

--- cigionline/static/js/cigionline.js
+++ cigionline/static/js/cigionline.js
@@ -1,4 +1,20 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
-let lol = '1'
+// MAIN NAVIGATION SCROLL
+
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+
+    if (scrollTop >= 80) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 80) {
+      $('#global-nav').removeClass('scrolled-nav');
+    }
+
+  });
+
+});
