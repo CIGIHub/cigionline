cadb53fbe1d7c2b5565880225be09bb2d186f0af
franciscaluz <vfranciscaluz@gmail.com>
2020-10-06 09:54:02 -0400

    CIGI-198 test add jquery to eslintrc file

--- cigionline/static/index.js
+++ cigionline/static/index.js
@@ -1,26 +1,25 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './css/cigionline.scss';
-(function($) {
-  // MAIN NAVIGATION SCROLL
-  $(document).ready(function() {
-    let scrollTop = 0;
-    $(window).scroll(function() {
-      scrollTop = $(window).scrollTop();
-      $('.counter').html(scrollTop);
 
-      if (scrollTop >= 66) {
-        $('#global-nav').addClass('scrolled-nav');
-      } else if (scrollTop < 66) {
-        $('#global-nav').removeClass('scrolled-nav');
-      }
-    });
+// MAIN NAVIGATION SCROLL
+$(document).ready(function() {
+  let scrollTop = 0;
+  $(window).scroll(function() {
+    scrollTop = $(window).scrollTop();
+    $('.counter').html(scrollTop);
+
+    if (scrollTop >= 66) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 66) {
+      $('#global-nav').removeClass('scrolled-nav');
+    }
   });
-  // SEARCH BAR OPEN
-  $(document).ready(function() {
-    $('#open-search-btn').click(function() {
-      $(this).toggleClass('open');
-      $('#popup-search').toggleClass('opened-popup');
-      $('body').toggleClass('disable-scroll');
-    });
+});
+// SEARCH BAR OPEN
+$(document).ready(function() {
+  $('#open-search-btn').click(function() {
+    $(this).toggleClass('open');
+    $('#popup-search').toggleClass('opened-popup');
+    $('body').toggleClass('disable-scroll');
   });
-})(jQuery);
+});
