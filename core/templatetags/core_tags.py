9a0a4f010ef814ddfe62b230013952be86a569b4
Natasha Scott <namscott@soniccat.com>
2020-12-03 15:58:56 -0500

    adding social links template and styling and download styling

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def no_protocol(value):
     return value.replace('http://', '').replace('https://', '')
+
+@register.filter
+def social_string(value):
+    return value.replace(' ', '+')
+    
