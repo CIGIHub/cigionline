42bd7edc80ce40cf6a1badcf5e453f42e77514ba
Natasha Scott <namscott@soniccat.com>
2020-12-04 10:43:35 -0500

    fixing lint issues

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -6,7 +6,9 @@ register = template.Library()
 def no_protocol(value):
     return value.replace('http://', '').replace('https://', '')
 
+
 @register.filter
 def social_string(value):
     return value.replace(' ', '+')
-    
+
+  
--- publications/models.py
+++ publications/models.py
@@ -98,6 +98,7 @@ class PublicationListPageFeaturedPublication(Orderable):
         ),
     ]
 
+
 class PublicationPage(
     BasicPageAbstract,
     ContentPage,
