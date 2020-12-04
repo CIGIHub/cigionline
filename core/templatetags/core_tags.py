72b91ebfbdb91f292b102893af9c297c711c124c
johnbxu <johnbxu@gmail.com>
2020-12-04 13:18:12 -0500

    Adjust people list logic

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -7,6 +7,16 @@ def no_protocol(value):
     return value.replace('http://', '').replace('https://', '')
 
 
+@register.filter
+def page_type(page):
+    return page._meta.verbose_name.lower().replace(' ', '-')
+
+
+@register.filter
+def in_list(value, the_list):
+    return value in the_list.split(',')
+
+
 @register.simple_tag
-def define(val=None):
-    return val
+def define(value):
+    return value
