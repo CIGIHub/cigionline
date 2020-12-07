ed84b07b43dd1aba6c2d3b85cf93db13b33f5a11
johnbxu <johnbxu@gmail.com>
2020-12-07 11:33:01 -0500

    Refactored pagination script as module

--- cigionline/static/multimedia_list_page.js
+++ cigionline/static/multimedia_list_page.js
@@ -1,30 +1,5 @@
+import paginate from './js/paginate_numbered';
+
 jQuery(function() {
-  const multimediaList = $('#multimedia-list-section');
-  const loaderSpinner = $('.loader-spinner');
-
-  function paginate() {
-    const pagination = $('.pagination-links-numbered');
-    pagination.find('a').on('click', function(event) {
-      const page = $(this).attr('data-page');
-
-      event.preventDefault();
-      loaderSpinner.addClass('show');
-
-      $.ajax({
-        url: '/multimedia/',
-        method: 'get',
-        data: { page },
-        success(response) {
-          multimediaList.empty().append(response);
-          paginate();
-          loaderSpinner.removeClass('show');
-          document.querySelector('#multimedia-list-section').scrollIntoView({
-            behavior: 'smooth',
-          });
-        },
-      });
-    });
-  }
-
-  paginate();
+  paginate('#multimedia-list-section', '/multimedia/');
 });
