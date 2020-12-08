aec1cafa1532b319dbc52e79180881c297e74fef
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-08 10:36:37 -0500

    cleanup include

--- cigionline/static/multimedia_list_page.js
+++ cigionline/static/multimedia_list_page.js
@@ -5,5 +5,5 @@ import SearchTable from './js/components/SearchTable';
 
 ReactDOM.render(
   <SearchTable endpoint="/multimedia" limit={18} fields="title,url,publishing_date,topics(title,url),image_hero_url,speakers" containerClass={['row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'multimedia-list-row']} RowComponent={MultimediaListing} />,
-  document.getElementById('multimedia-listing'),
+  document.getElementById('multimedia-search-table'),
 );
