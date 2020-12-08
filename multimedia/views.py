82a8ed99072526810279ba2c31ca10bdaead49d3
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-08 10:33:57 -0500

    cleanup

--- multimedia/views.py
+++ multimedia/views.py
@@ -1,14 +1,8 @@
-from rest_framework.pagination import PageNumberPagination
 from wagtail.api.v2.filters import OrderingFilter
 from wagtail.api.v2.serializers import PageSerializer
 from wagtail.api.v2.views import BaseAPIViewSet
 
 from .models import MultimediaPage
-
-
-class MultimediaResultsSetPagination(PageNumberPagination):
-    page_size = 18
-    max_page_size = 18
 
 
 class MultimediaPageViewSet(BaseAPIViewSet):
@@ -20,6 +14,3 @@ class MultimediaPageViewSet(BaseAPIViewSet):
 
     def get_queryset(self):
         return self.model.objects.all().public().live().order_by('-publishing_date')
-    # queryset = MultimediaPage.objects.live().order_by('-publishing_date')
-    # serializer_class = MultimediaPageSerializer
-    # pagination_class = MultimediaResultsSetPagination
