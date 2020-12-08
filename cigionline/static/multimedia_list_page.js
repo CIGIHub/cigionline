8e204214bc3e22d70fb601309c0c26d391b24a7f
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-08 13:33:08 -0500

    migration and formatting

--- cigionline/static/multimedia_list_page.js
+++ cigionline/static/multimedia_list_page.js
@@ -4,6 +4,18 @@ import MultimediaListing from './js/components/MultimediaListing';
 import SearchTable from './js/components/SearchTable';
 
 ReactDOM.render(
-  <SearchTable endpoint="/multimedia" limit={18} fields="title,url,publishing_date,topics(title,url),image_hero_url,speakers" containerClass={['row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'multimedia-list-row']} RowComponent={MultimediaListing} />,
+  <SearchTable
+    endpoint="/multimedia"
+    limit={18}
+    fields="title,url,publishing_date,topics(title,url),image_hero_url,speakers"
+    containerClass={[
+      'row',
+      'row-cols-1',
+      'row-cols-sm-2',
+      'row-cols-md-3',
+      'multimedia-list-row',
+    ]}
+    RowComponent={MultimediaListing}
+  />,
   document.getElementById('multimedia-search-table'),
 );
