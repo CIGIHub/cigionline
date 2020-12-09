789f3502f30bfee1bf2f90d846a4fcd82560cdfa
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-09 12:14:31 -0500

    test title, url, and publishing date

--- multimedia/tests.py
+++ multimedia/tests.py
@@ -74,5 +74,88 @@ class MultimediaPageViewSetTests(WagtailPageTests):
         home_page.save()
 
     def test_page_1_query_returns_200(self):
-        response = self.client.get('/api/multimedia/')
-        self.assertEqual(response.status_code, 200)
+        res = self.client.get('/api/multimedia/?limit=18&offset=0&fields=title,url,publishing_date,topics(title,url)')
+        self.assertEqual(res.status_code, 200)
+        resJson = res.json()
+        self.assertEqual(resJson['meta']['total_count'], 30)
+        self.assertEqual(len(resJson['items']), 18)
+
+        resExpected = [{
+            'publishing_date': '2020-12-25T08:00:00-05:00',
+            'title': 'Test Multimedia 30',
+            'url': '/multimedia/multimedia-30/',
+        }, {
+            'publishing_date': '2020-12-16T08:00:00-05:00',
+            'title': 'Test Multimedia 29',
+            'url': '/multimedia/multimedia-29/',
+        }, {
+            'publishing_date': '2020-12-11T08:00:00-05:00',
+            'title': 'Test Multimedia 28',
+            'url': '/multimedia/multimedia-28/',
+        }, {
+            'publishing_date': '2020-12-10T08:00:00-05:00',
+            'title': 'Test Multimedia 27',
+            'url': '/multimedia/multimedia-27/',
+        }, {
+            'publishing_date': '2020-12-07T08:00:00-05:00',
+            'title': 'Test Multimedia 26',
+            'url': '/multimedia/multimedia-26/',
+        }, {
+            'publishing_date': '2020-11-24T08:00:00-05:00',
+            'title': 'Test Multimedia 25',
+            'url': '/multimedia/multimedia-25/',
+        }, {
+            'publishing_date': '2020-11-05T08:00:00-05:00',
+            'title': 'Test Multimedia 24',
+            'url': '/multimedia/multimedia-24/',
+        }, {
+            'publishing_date': '2020-10-28T08:00:00-04:00',
+            'title': 'Test Multimedia 23',
+            'url': '/multimedia/multimedia-23/',
+        }, {
+            'publishing_date': '2020-10-27T08:00:00-04:00',
+            'title': 'Test Multimedia 22',
+            'url': '/multimedia/multimedia-22/',
+        }, {
+            'publishing_date': '2020-10-21T08:00:00-04:00',
+            'title': 'Test Multimedia 21',
+            'url': '/multimedia/multimedia-21/',
+        }, {
+            'publishing_date': '2020-10-15T08:00:00-04:00',
+            'title': 'Test Multimedia 20',
+            'url': '/multimedia/multimedia-20/',
+        }, {
+            'publishing_date': '2020-09-14T08:00:00-04:00',
+            'title': 'Test Multimedia 19',
+            'url': '/multimedia/multimedia-19/',
+        }, {
+            'publishing_date': '2020-09-03T08:00:00-04:00',
+            'title': 'Test Multimedia 18',
+            'url': '/multimedia/multimedia-18/',
+        }, {
+            'publishing_date': '2020-08-14T08:00:00-04:00',
+            'title': 'Test Multimedia 17',
+            'url': '/multimedia/multimedia-17/',
+        }, {
+            'publishing_date': '2020-08-12T08:00:00-04:00',
+            'title': 'Test Multimedia 16',
+            'url': '/multimedia/multimedia-16/',
+        }, {
+            'publishing_date': '2020-08-11T08:00:00-04:00',
+            'title': 'Test Multimedia 15',
+            'url': '/multimedia/multimedia-15/',
+        }, {
+            'publishing_date': '2020-08-06T08:00:00-04:00',
+            'title': 'Test Multimedia 14',
+            'url': '/multimedia/multimedia-14/',
+        }, {
+            'publishing_date': '2020-07-27T08:00:00-04:00',
+            'title': 'Test Multimedia 13',
+            'url': '/multimedia/multimedia-13/',
+        }]
+
+        for i in range(18):
+            self.assertEqual(resJson['items'][i]['publishing_date'], resExpected[i]['publishing_date'])
+            self.assertEqual(resJson['items'][i]['title'], resExpected[i]['title'])
+            self.assertEqual(resJson['items'][i]['url'], resExpected[i]['url'])
+        # print(response.json())
