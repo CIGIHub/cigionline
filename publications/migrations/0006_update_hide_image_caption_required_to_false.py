9a5e2a7923806f3b1606d48f2b4bc00572599cd3
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-09 18:36:39 -0500

    fix migrations

--- publications/migrations/0006_update_hide_image_caption_required_to_false.py
+++ publications/migrations/0006_update_hide_image_caption_required_to_false.py
@@ -11,7 +11,7 @@ import wagtail.images.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('publications', '0003_create_chart_stream_block'),
+        ('publications', '0005_create_pdf_download_block'),
     ]
 
     operations = [
