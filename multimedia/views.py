29ed66c0070736d1689fe55e254116b8c99e7ed8
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-09 10:05:50 -0500

    add search and filter

--- multimedia/views.py
+++ multimedia/views.py
@@ -1,4 +1,4 @@
-from wagtail.api.v2.filters import OrderingFilter
+from wagtail.api.v2.filters import FieldsFilter, OrderingFilter, SearchFilter
 from wagtail.api.v2.serializers import PageSerializer
 from wagtail.api.v2.views import BaseAPIViewSet
 
@@ -9,7 +9,9 @@ class MultimediaPageViewSet(BaseAPIViewSet):
     model = MultimediaPage
     base_serializer_class = PageSerializer
     filter_backends = [
+        FieldsFilter,
         OrderingFilter,
+        SearchFilter,
     ]
 
     def get_queryset(self):
