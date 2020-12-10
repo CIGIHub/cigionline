98de9e23256b4263ac9b39f61db062c0a32de17a
Jonathan Xu <johnbxu@gmail.com>
2020-12-09 19:58:16 -0500

    GIGI-201: Create large feature template (#536)
    * Added landing page and feature large templates
    
    * Changed featured article relation to access article page properties
    
    * Adjust people list logic
    
    * style and create templates for title, subtitle, topic list and authors
    
    * Add migrations files
    
    * factor out people list
    
    * factor out topics
    
    * Add media breakpoint
    
    * Add margin auto to center-text
    
    * Remove comment
    
    * fix migrations
    
    * Put logic in sub templates
    
    * Break up feature scss into two files
    
    * Add scss imports and fix date format
    
    * Mix migrations
    
    * fix migrations
    
    * fix linting
    
    Co-authored-by: Gabriel Tan-Chen <gabriel@gabrieltanchen.com>

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -8,5 +8,20 @@ def no_protocol(value):
 
 
 @register.filter
+def page_type(page):
+    return page._meta.verbose_name.lower().replace(' ', '-')
+
+
+@register.filter
+def in_list(value, the_list):
+    return value in the_list.split(',')
+
+
+@register.simple_tag
+def define(value):
+    return value
+
+
+@register.filter
 def social_string(value):
     return value.replace(' ', '+')
