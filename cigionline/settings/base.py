34155eed7542444d68ff5e158aa7fda193d6acd3
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-11 23:17:22 -0500

    topic view set test

--- cigionline/settings/base.py
+++ cigionline/settings/base.py
@@ -202,7 +202,7 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "cigionline"
-WAGTAILAPI_LIMIT_MAX = 24
+WAGTAILAPI_LIMIT_MAX = 50
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
