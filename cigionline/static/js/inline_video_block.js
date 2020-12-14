870135b1f3a7f0b746c02eec42f2e8bf690ef987
johnbxu <johnbxu@gmail.com>
2020-12-14 08:44:06 -0500

    Add inline video js

--- cigionline/static/js/inline_video_block.js
+++ cigionline/static/js/inline_video_block.js
@@ -1,5 +1,26 @@
 export default function addInlineVideoActions() {
   const inlineVideoBlocks = document.getElementsByClassName('stream-block-inline-video');
 
+  for (const block of inlineVideoBlocks) {
+    const watchLabel = block.getElementsByClassName('stream-block-inline-video-label')[0];
+    const blockImage = block.getElementsByClassName('img-wrapper')[0];
+    const blockVideo = block.getElementsByClassName('video-wrapper')[0];
+    watchLabel.addEventListener('click', function () {
+      if (blockImage.classList.contains('shrink')) {
+        blockVideo.classList.add('shrink');
+        window.setTimeout(function() {
+          blockImage.classList.remove('shrink');
+        }, 1000)
+      } else {
+        blockImage.classList.add('shrink');
+        window.setTimeout(function() {
+          blockVideo.classList.remove('shrink');
+        }, 1000)
+      }
+    });
+
+  }
+
+
   console.log(inlineVideoBlocks);
 }
