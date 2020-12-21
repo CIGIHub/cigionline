3066edb02f18050d5728ff6a650904b276ce53d2
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-21 10:15:07 -0500

    style video header

--- cigionline/static/pages/multimedia_page/index.js
+++ cigionline/static/pages/multimedia_page/index.js
@@ -1,25 +1,31 @@
 import './css/multimedia_page.scss';
 
-const multimediaHeroVideo = document.querySelector('.multimedia-hero-video');
+const mmHero = document.querySelector('.mm-hero');
+const mmVideoContainer = document.querySelector('.mm-video-container');
+const mmVideoHeader = document.querySelector('.mm-video-header');
 
-if (multimediaHeroVideo) {
+if (mmHero && mmVideoContainer && mmVideoHeader) {
   window.addEventListener('scroll', function() {
     const windowWidth = parseFloat(getComputedStyle(document.querySelector('html'), null).width.replace('px', ''));
     const windowHeight = parseFloat(getComputedStyle(document.querySelector('html'), null).height.replace('px', ''));
 
     const scrollTop = document.querySelector('html').scrollTop;
-    const multimediaHeroVideoHeight = parseFloat(getComputedStyle(multimediaHeroVideo, null).height.replace('px', ''));
-    const multimediaHeroVideoTop = multimediaHeroVideo.offsetTop;
-    const multimediaHeroVideoBottom = multimediaHeroVideoTop + multimediaHeroVideoHeight;
+    const multimediaHeroVideoHeight = parseFloat(getComputedStyle(mmVideoHeader, null).height.replace('px', ''));
+
+    const mmHeroHeight = parseFloat(getComputedStyle(mmHero, null).height.replace('px', ''));
+    const mmHeroTop = mmHero.offsetTop;
+    const mmHeroBottom = mmHeroTop + mmHeroHeight;
 
     if (windowWidth > 768
         && windowHeight > 600
-        && scrollTop > multimediaHeroVideoBottom) {
-      if (!multimediaHeroVideo.classList.contains('sticky-video')) {
-        multimediaHeroVideo.classList.add('sticky-video');
+        && scrollTop > mmHeroBottom) {
+      if (!mmVideoContainer.classList.contains('sticky-video')) {
+        mmVideoContainer.style.height = `${multimediaHeroVideoHeight}px`;
+        mmVideoContainer.classList.add('sticky-video');
       }
-    } else if (multimediaHeroVideo.classList.contains('sticky-video')) {
-      multimediaHeroVideo.classList.remove('sticky-video');
+    } else if (mmVideoContainer.classList.contains('sticky-video')) {
+      mmVideoContainer.style.height = '';
+      mmVideoContainer.classList.remove('sticky-video');
     }
   });
 }
