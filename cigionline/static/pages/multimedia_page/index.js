fd81b1414510b4f2667718f97b8c31ad0d77d8cb
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-22 14:20:38 -0500

    Create hero section for multimedia pages (#622)
    * start hero with title and topics
    
    * add meta and social links
    
    * add video embed
    
    * add javascript to add sticky class
    
    * style video header
    
    * add animation keyframes
    
    * organize scss
    
    * fix sass linter
    
    * move slide down to animations scss file
    
    * move sticky header js logic to function

--- cigionline/static/pages/multimedia_page/index.js
+++ cigionline/static/pages/multimedia_page/index.js
@@ -1,1 +1,14 @@
 import './css/multimedia_page.scss';
+import createStickyHeaderScrollListener from '../../js/create_sticky_header_scroll_listener';
+
+const mmHero = document.querySelector('.mm-hero');
+const mmVideoContainer = document.querySelector('.mm-video-container');
+const mmVideoHeader = document.querySelector('.mm-video-header');
+
+if (mmHero && mmVideoContainer && mmVideoHeader) {
+  createStickyHeaderScrollListener({
+    scrollTriggerEl: mmHero,
+    stickyHeaderContainerEl: mmVideoContainer,
+    stickyHeaderEl: mmVideoHeader,
+  });
+}
