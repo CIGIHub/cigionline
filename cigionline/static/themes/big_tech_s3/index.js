18fa5ed74dede8779f7a2c3561baf3aa8ad44cf0
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-12-21 21:48:21 -0500

    use create sticky header method

--- cigionline/static/themes/big_tech_s3/index.js
+++ cigionline/static/themes/big_tech_s3/index.js
@@ -1,27 +1,13 @@
 import './css/big_tech_s3.scss';
+import createStickyHeaderScrollListener from '../../js/create_sticky_header_scroll_listener';
 
 const simplecastContainer = document.querySelector('.simplecast-container');
 const simplecastHeader = document.querySelector('.simplecast-header');
 
 if (simplecastContainer && simplecastHeader) {
-  window.addEventListener('scroll', function() {
-    const windowWidth = parseFloat(getComputedStyle(document.querySelector('html'), null).width.replace('px', ''));
-    const windowHeight = parseFloat(getComputedStyle(document.querySelector('html'), null).height.replace('px', ''));
-
-    const scrollTop = document.querySelector('html').scrollTop;
-
-    const containerHeight = parseFloat(getComputedStyle(simplecastContainer, null).height.replace('px', ''));
-    const containerTop = simplecastContainer.offsetTop;
-    const containerBottom = containerTop + containerHeight;
-
-    if (windowWidth > 768
-        && windowHeight > 600
-        && scrollTop > containerBottom) {
-      if (!simplecastHeader.classList.contains('sticky-video')) {
-        simplecastHeader.classList.add('sticky-video');
-      }
-    } else if (simplecastHeader.classList.contains('sticky-video')) {
-      simplecastHeader.classList.remove('sticky-video');
-    }
+  createStickyHeaderScrollListener({
+    scrollTriggerEl: simplecastContainer,
+    stickyHeaderContainerEl: simplecastContainer,
+    stickyHeaderEl: simplecastHeader,
   });
 }
