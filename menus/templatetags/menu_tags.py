17ad6fd04bcba8a5585c15cd014acabf230efb4b
johnbxu <johnbxu@gmail.com>
2021-01-05 11:46:04 -0500

    Sorted submenus based on orderable order

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -8,16 +8,3 @@ register = template.Library()
 @register.simple_tag()
 def get_menu_items(slug):
     return Menu.objects.get(slug=slug).menu_items.all
-
-@register.simple_tag()
-def get_dropdown_menus():
-    menus = [
-      'Research',
-      'Publications',
-      'Experts',
-      'Opinions',
-      'Multimedia',
-      'Events',
-      'About',
-    ]
-    return Menu.objects.filter(name__in=menus)
