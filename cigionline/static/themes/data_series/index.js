ad0254dc8943a9e306bdb37c7f9c61ece33dd44f
Natasha Scott <namscott@soniccat.com>
2021-01-13 11:44:43 -0500

    adding sticky header for data articles

--- cigionline/static/themes/data_series/index.js
+++ cigionline/static/themes/data_series/index.js
@@ -1,1 +1,21 @@
 import './css/data_series.scss';
+//import createStickyHeaderScrollListener from '../../js/create_sticky_header_scroll_listener';
+
+const headerEl = document.querySelector('header');
+const stickyHeader = document.querySelector('.article-header-sticky');
+var headerHeight = null;
+
+window.addEventListener('load', function() {
+  headerHeight = headerEl.offsetHeight
+  
+});
+
+window.addEventListener('scroll', function(){
+  var scrollTop = document.querySelector('html').scrollTop;
+  if (scrollTop > headerHeight){
+    stickyHeader.classList.add('scrolled');
+  }
+  else {
+    stickyHeader.classList.remove('scrolled');
+  }
+});
