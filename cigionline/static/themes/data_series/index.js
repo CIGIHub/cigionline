72e7ea5a73e26779f59bf78d2dc8ab2b00a84686
Natasha Scott <namscott@soniccat.com>
2021-01-13 15:42:13 -0500

    fixing lint errors

--- cigionline/static/themes/data_series/index.js
+++ cigionline/static/themes/data_series/index.js
@@ -2,24 +2,23 @@ import './css/data_series.scss';
 
 const headerEl = document.querySelector('header');
 const stickyHeader = document.querySelector('.article-header-sticky');
-var headerHeight, docHeight = null;
+let headerHeight =  null;
+let docHeight = null;
 
 window.addEventListener('load', function() {
   headerHeight = headerEl.offsetHeight;
   docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 });
 
-window.addEventListener('scroll', function(){
-  var scrollTop = document.querySelector('html').scrollTop;
+window.addEventListener('scroll', function() {
+  let scrollTop = document.querySelector('html').scrollTop;
 
-  var scrolled = (scrollTop/docHeight) * 100;
-  document.querySelector('progress').style.width = scrolled + "%";
+  let scrolled = (scrollTop / docHeight) * 100;
+  document.querySelector('progress').style.width = scrolled + '%';
 
-  if (scrollTop > headerHeight){
-    stickyHeader.classList.add('scrolled');
-  }
+  if (scrollTop > headerHeight) {
+    stickyHeader.classList.add('scrolled'); }
   else {
-    stickyHeader.classList.remove('scrolled');
-  }
+    stickyHeader.classList.remove('scrolled'); }
 
 });
