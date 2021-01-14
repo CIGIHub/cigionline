81ebcfa9149271ca0398e04133c91adf05b42168
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-01-13 22:05:11 -0500

    article series page

--- cigionline/static/themes/platform_governance_series/index.js
+++ cigionline/static/themes/platform_governance_series/index.js
@@ -1,1 +1,32 @@
 import './css/platform_governance_series.scss';
+
+console.log('hello world');
+
+const displayGridButton = document.querySelector('.platform-governance-display-grid');
+const displayListButton = document.querySelector('.platform-governance-display-list');
+const listingContainer = document.querySelector('.platform-governance-listing');
+
+if (displayGridButton && displayListButton && listingContainer) {
+  displayGridButton.addEventListener('click', function() {
+    console.log('displayGridButton click');
+    console.log(displayGridButton.classList);
+    console.log(displayGridButton.classList.contains('active'));
+    if (!displayGridButton.classList.contains('active')) {
+      displayGridButton.classList.add('active');
+      displayListButton.classList.remove('active');
+      listingContainer.classList.remove('display-list');
+    }
+  });
+  displayListButton.addEventListener('click', function() {
+    console.log('displayListButton click');
+    console.log(displayListButton.classList);
+    console.log(displayListButton.classList.contains('active'));
+    if (!displayListButton.classList.contains('active')) {
+      displayListButton.classList.add('active');
+      displayGridButton.classList.remove('active');
+      listingContainer.classList.add('display-list');
+    }
+  });
+} else {
+  console.log('fail');
+}
