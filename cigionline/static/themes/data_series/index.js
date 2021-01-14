f96f9439a9c4504b7c46f96bcecbafd5c4a3794c
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-01-14 09:31:09 -0500

    fix progress styling

--- cigionline/static/themes/data_series/index.js
+++ cigionline/static/themes/data_series/index.js
@@ -15,7 +15,7 @@ window.addEventListener('scroll', function() {
   const scrollTop = document.querySelector('html').scrollTop;
 
   scrolled = (scrollTop / docHeight) * 100;
-  document.querySelector('progress').style.width = `${scrolled}%`;
+  document.querySelector('progress').setAttribute('value', scrolled);
 
   if (scrollTop > headerHeight) {
     stickyHeader.classList.add('scrolled');
