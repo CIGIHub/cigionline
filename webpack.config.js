d334afab907be66cba19a3847e2f0ec61fc257cf
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-01-20 17:54:25 -0500

    Implement paginated table on Opinion Series page (#817)

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ const config = {
   entry: {
     articleLandingPage: './cigionline/static/pages/article_landing_page/index.js',
     articlePage: './cigionline/static/pages/article_page/index.js',
+    articleSeriesListPage: './cigionline/static/pages/article_series_list_page/index.js',
     cigionline: './cigionline/static/index.js',
     eventListPage: './cigionline/static/pages/event_list_page/index.js',
     eventPage: './cigionline/static/pages/event_page/index.js',
