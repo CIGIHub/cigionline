5eda1170a0faaf0913f3a5f65fd0cd33a2824049
Natasha Scott <namscott@soniccat.com>
2021-01-20 11:44:39 -0500

    styling drop caps, external quote, blockcquote

--- cigionline/static/themes/cyber_series/index.js
+++ cigionline/static/themes/cyber_series/index.js
@@ -1,1 +1,15 @@
 import './css/cyber_series.scss';
+
+const blockquoteEl = document.querySelector('stream-block-blockquote');
+
+
+// window.addEventListener('load', function() {
+//   headerHeight = headerEl.offsetHeight;
+//   docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// });
+
+// window.addEventListener('scroll', function() {
+//   const scrollTop = document.querySelector('html').scrollTop;
+
+// });
+
