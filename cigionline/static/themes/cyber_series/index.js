97eb1c23c27fa8d8b273a9e4d4e10d3340b9c919
Natasha Scott <namscott@soniccat.com>
2021-01-21 15:33:58 -0500

    cyberseries landing page body styling

--- cigionline/static/themes/cyber_series/index.js
+++ cigionline/static/themes/cyber_series/index.js
@@ -1,1 +1,19 @@
 import './css/cyber_series.scss';
+
+const firstBlock = $('.cyber-series-article-series-body .block-paragraph').first();
+const lastBlock = $('.cyber-series-article-series-body .block-paragraph').last();
+
+lastBlock.hide();
+
+let readMore = firstBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-down"></i></span>');
+let readLess = lastBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-up"></i></span>');
+
+readMore.on('click', function(){
+  firstBlock.find('.read-more').hide();
+  lastBlock.show();
+});
+
+readLess.on('click', function(){
+  firstBlock.find('.read-more').show();
+  lastBlock.hide();
+});
