39e280c3414065f17f3af7dfcb56e6abc678d021
Natasha Scott <namscott@soniccat.com>
2021-01-21 15:46:21 -0500

    fixing lint errors and column width for about

--- cigionline/static/themes/cyber_series/index.js
+++ cigionline/static/themes/cyber_series/index.js
@@ -5,15 +5,15 @@ const lastBlock = $('.cyber-series-article-series-body .block-paragraph').last()
 
 lastBlock.hide();
 
-let readMore = firstBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-down"></i></span>');
-let readLess = lastBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-up"></i></span>');
+const readMore = firstBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-down"></i></span>');
+const readLess = lastBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-up"></i></span>');
 
-readMore.on('click', function(){
+readMore.on('click', function() {
   firstBlock.find('.read-more').hide();
   lastBlock.show();
 });
 
-readLess.on('click', function(){
+readLess.on('click', function() {
   firstBlock.find('.read-more').show();
   lastBlock.hide();
 });
