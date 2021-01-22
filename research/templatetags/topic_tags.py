c584018c7f60ec265757d04343a7d1e79590e3f4
johnbxu <johnbxu@gmail.com>
2021-01-22 13:45:30 -0500

    Add highlighted topics to research page

--- research/templatetags/topic_tags.py
+++ research/templatetags/topic_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.inclusion_tag('research/highlighted_topics.html')
 def highlighted_topics():
     highlighted_topics = TopicPage.objects.live().filter(archive=0).order_by('title')
-    annotated_topics = TopicPage.objects.live().filter(archive=0).annotate(count=Count('contentpage'))
+    annotated_topics = TopicPage.objects.live().filter(archive=0).order_by('title').annotate(count=Count('contentpage'))
     return {'highlighted_topics': highlighted_topics, 'annotated_topics': annotated_topics}
 
 
@@ -16,8 +16,3 @@ def highlighted_topics():
 def topics(topics):
     topics = topics.live().order_by('title')
     return {'topics': topics}
-
-
-# @register.simple_tag
-# def topic_content(topic):
-#     return ContentPage.objects.live().filter(topics__contains=topic)
