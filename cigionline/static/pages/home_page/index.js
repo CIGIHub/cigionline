dbc2d956a1d544307e7906faaf7e5dd0338b652e
Jonathan Xu <johnbxu@gmail.com>
2021-01-25 14:58:17 -0500

    CIGI-408: Add home page highlights section (#848)
    * Add home_page files
    
    * Add swiper package
    
    * Configure and add markup for swiper
    
    * Style poster teaser
    
    * fix spacing and border issues
    
    * Add article series authors property
    
    * Add opinion-series check for feature content template authors
    
    * Add animated gif support to poster teaser
    
    * adjust swiper controls for small screens
    
    * Update gif file paths
    
    * Adjust subheading margin
    
    * Remove unneeded css
    
    * Add eslint disable
    
    * Update package.json
    
    * move swiper css to home page bundle
    
    Co-authored-by: Gabriel Tan-Chen <gabriel@gabrieltanchen.com>

---
+++ cigionline/static/pages/home_page/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+
+import 'swiper/swiper-bundle.css';
+import './css/_home_page.scss';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// Homepage Highlights
+Swiper.use([Navigation, Pagination]);
+const swiperContainer = document.querySelector('.swiper-container');
+
+if (swiperContainer) {
+  const swiperControls = swiperContainer.querySelector('.swiper-controls');
+  const homepageSwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      992: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
+    },
+  });
+
+  if (swiperContainer.querySelectorAll('.swiper-slide').length <= 4) {
+    swiperControls.style.display = 'none';
+  }
+}
