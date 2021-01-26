4c98702188e92e946b072d733ab90c4dbba50753
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-01-26 09:42:38 -0500

    remove console statement

--- cigionline/static/themes/cyber_series/index.js
+++ cigionline/static/themes/cyber_series/index.js
@@ -4,7 +4,6 @@ import './css/cyber_series.scss';
 const firstBlock = $('.cyber-series-article-series-body .body .container').first();
 const lastBlock = $('.cyber-series-article-series-body .body .container').last();
 
-console.log(lastBlock);
 lastBlock.hide();
 
 firstBlock.find('p').last().append('<span class="read-more"><i class="fa fa-chevron-down"></i></span>');
