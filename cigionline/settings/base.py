80c37461657afc2f6006733f2f5b052b234db856
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-01-29 08:28:17 -0500

    Increase api max limit

--- cigionline/settings/base.py
+++ cigionline/settings/base.py
@@ -252,7 +252,7 @@ MEDIA_URL = '/staticmedia/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "cigionline"
-WAGTAILAPI_LIMIT_MAX = 40
+WAGTAILAPI_LIMIT_MAX = 100
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
