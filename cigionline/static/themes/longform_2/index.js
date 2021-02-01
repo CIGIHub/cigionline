d866c46cc68e83cf237bb4bebe796a29a9120642
johnbxu <johnbxu@gmail.com>
2021-02-01 15:40:02 -0500

    Add script and styling

--- cigionline/static/themes/longform_2/index.js
+++ cigionline/static/themes/longform_2/index.js
@@ -1,2 +1,62 @@
+/* eslint-disable no-unused-vars */
+
 import Panzoom from '@panzoom/panzoom';
 import './css/longform_2.scss';
+
+const panzoomContainers = Array.from(document.getElementsByClassName('stream-image-scroll-block'));
+const panzoomImg = [];
+const panzoomScrollbar = [];
+const panzoomArrow = [];
+const panzoomScrollbarWrapper = [];
+
+function setScrollBarWidth(index) {
+  panzoomScrollbar[index].style.width = `${Math.min((panzoomContainers[index].offsetWidth / panzoomImg[index].offsetWidth) * 100, 100)}%`;
+}
+
+function moveScrollBar(x, index) {
+  panzoomScrollbar[index].style.transform = `matrix(1,0,0,1,${x},0)`;
+}
+
+function moveImage(x, index) {
+  panzoomImg[index].style.transform = `matrix(1,0,0,1,${x},0)`;
+}
+
+function resizeUpdate() {
+  panzoomContainers.forEach(function(panzoomContainer, index) {
+    setScrollBarWidth(index);
+    if (panzoomImg[index].offsetWidth <= panzoomContainer.offsetWidth) {
+      panzoomScrollbarWrapper[index].style.display = 'none';
+      panzoomArrow[index].style.display = 'none';
+    } else {
+      panzoomScrollbarWrapper[index].style.display = 'block';
+      panzoomArrow[index].style.display = 'block';
+    }
+  });
+}
+
+panzoomContainers.forEach(function(panzoomContainer, index) {
+  panzoomImg[index] = panzoomContainer.querySelector('img');
+  panzoomScrollbar[index] = panzoomContainer.querySelector('.panzoom-scrollbar-inner');
+  panzoomScrollbarWrapper[index] = panzoomContainer.querySelector('.panzoom-scrollbar-outer');
+  panzoomArrow[index] = panzoomContainer.querySelector('.panzoom-arrow');
+  const panzoomImgInstance = Panzoom(panzoomImg[index], { contain: 'outside', disableYAxis: true });
+  const panzoomScrollbarInstance = Panzoom(panzoomScrollbar[index], { contain: 'inside', disableYAxis: true });
+
+  setScrollBarWidth(index);
+
+  panzoomImg[index].addEventListener('panzoompan', function(event) {
+    const x = Math.abs(
+      (panzoomScrollbar[index].offsetWidth / panzoomContainers[index].offsetWidth) * event.detail.x,
+    );
+    moveScrollBar(x, index);
+  });
+
+  panzoomScrollbar[index].addEventListener('panzoompan', function(event) {
+    const x = Math.abs(
+      (panzoomContainers[index].offsetWidth / panzoomScrollbar[index].offsetWidth) * event.detail.x,
+    ) * -1;
+    moveImage(x, index);
+  });
+});
+
+window.addEventListener('resize', resizeUpdate);
