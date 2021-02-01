437fb7e1799d59fae851ffb434ed899547a71597
Jonathan Xu <johnbxu@gmail.com>
2021-02-01 10:40:19 -0500

    CIGI-412: Implement home page promotion blocks (#862)
    * Add promotion block model
    
    * Fix models
    
    * Add fields to promotion block model
    
    * Add styling
    
    * Add title panel
    
    * Add margin on small screens
    
    * Fix spacing
    
    * Add migrations
    
    * Fix home test
    
    * Add hover effect
    
    * Add promotions tests
    
    * Update models
    
    * Change admin icon
    
    * Add fields to admin
    
    * Redo migrations
    
    * Rename title to name and redo migrations
    
    * linter
    
    * Fix home test
    
    * add image small panel
    
    Co-authored-by: Gabriel Tan-Chen <gabriel@gabrieltanchen.com>

--- cigionline/settings/base.py
+++ cigionline/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'multimedia',
     'newsletters',
     'people',
+    'promotions',
     'publications',
     'research',
     'search',
---
+++ promotions/tests.py
@@ -0,0 +1,10 @@
+from wagtail.tests.utils import WagtailPageTests
+
+from .models import PromotionBlock
+
+
+class PromotionBlockPageTests(WagtailPageTests):
+    def test_create_promotion_block(self):
+        test_block = PromotionBlock.objects.create(name="test1", block_type=PromotionBlock.PromotionBlockTypes.STANDARD, link_url="https://test.test")
+        self.assertTrue(isinstance(test_block, PromotionBlock))
+        self.assertEqual(str(test_block), test_block.name)
