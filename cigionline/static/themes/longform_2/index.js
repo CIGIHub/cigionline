0124cb7c30876ad38faeaae775c135b38e4bf751
Natasha Scott <namscott@soniccat.com>
2021-02-04 09:47:58 -0500

    desktop sticky header and hero

--- cigionline/static/themes/longform_2/index.js
+++ cigionline/static/themes/longform_2/index.js
@@ -60,3 +60,31 @@ panzoomContainers.forEach(function(panzoomContainer, index) {
 });
 
 window.addEventListener('resize', resizeUpdate);
+
+
+/* sticky header */
+
+const headerEl = document.querySelector('header');
+const stickyHeader = document.querySelector('.article-header-sticky');
+let headerHeight = null;
+let docHeight = null;
+let scrolled = null;
+
+window.addEventListener('load', function() {
+  headerHeight = headerEl.offsetHeight;
+  docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+});
+
+window.addEventListener('scroll', function() {
+  const scrollTop = document.querySelector('html').scrollTop;
+
+  scrolled = (scrollTop / docHeight) * 100;
+  document.querySelector('progress').setAttribute('value', scrolled);
+
+  if (scrollTop > headerHeight) {
+    stickyHeader.classList.add('scrolled');
+  } else {
+    stickyHeader.classList.remove('scrolled');
+  }
+});
+
