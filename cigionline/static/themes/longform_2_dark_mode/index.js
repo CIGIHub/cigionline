d8e66bda915aa4b8433a64d51b202556c001d2f8
Natasha Scott <namscott@soniccat.com>
2021-02-12 12:35:05 -0500

    sticky header

--- cigionline/static/themes/longform_2_dark_mode/index.js
+++ cigionline/static/themes/longform_2_dark_mode/index.js
@@ -1,2 +1,22 @@
 import './css/longform_2_dark_mode.scss';
 
+$(window).on('load', function() {
+  const headerHeight = $('header').height();
+  const scrollTop = $(window).scrollTop();
+  const docHeight = $(document).height();
+  const winHeight = $(window).height();
+
+  const maxHeight = docHeight - winHeight;
+  const scrolled = (scrollTop / maxHeight) * 100;
+
+  console.log('here');
+  console.log(headerHeight);
+
+  $('progress').attr('value', scrolled);
+
+  if (scrollTop > headerHeight) {
+    $('body').addClass('scrolled');
+  } else {
+    $('body').removeClass('scrolled');
+  }
+});
