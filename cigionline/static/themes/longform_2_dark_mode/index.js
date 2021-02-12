4d47c8ae539f6078d1ba6383333c03ec6bfe6a3e
Natasha Scott <namscott@soniccat.com>
2021-02-12 14:08:47 -0500

    Revert "fixing sticky header and hero"
    This reverts commit 49433fee15b9d69f5e7662232305dc9e72be0bdc.

--- cigionline/static/themes/longform_2_dark_mode/index.js
+++ cigionline/static/themes/longform_2_dark_mode/index.js
@@ -1,13 +1,17 @@
 import './css/longform_2_dark_mode.scss';
 
-let scrolled = null;
-let headerHeight = null;
-let maxHeight = null;
-let scrollTop = 0;
+$(window).on('load', function() {
+  const headerHeight = $('header').height();
+  const scrollTop = $(window).scrollTop();
+  const docHeight = $(document).height();
+  const winHeight = $(window).height();
 
-function setScrollPosition() {
-  scrollTop = $(window).scrollTop();
-  scrolled = (scrollTop / maxHeight) * 100;
+  const maxHeight = docHeight - winHeight;
+  const scrolled = (scrollTop / maxHeight) * 100;
+
+  console.log('here');
+  console.log(headerHeight);
+
   $('progress').attr('value', scrolled);
 
   if (scrollTop > headerHeight) {
@@ -15,18 +19,4 @@ function setScrollPosition() {
   } else {
     $('body').removeClass('scrolled');
   }
-}
-
-$(window).on('load', function() {
-  const docHeight = $(document).height();
-  const winHeight = $(window).height();
-  headerHeight = $('header').height();
-
-  maxHeight = docHeight - winHeight;
-
-  setScrollPosition();
-});
-
-$(window).on('scroll', function() {
-  setScrollPosition();
 });
