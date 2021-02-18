564dd32e1352f6128b24db461789c486fd294dc5
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2021-02-18 12:13:56 -0500

    clean up search view

--- search/views.py
+++ search/views.py
@@ -1,45 +1,22 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import JsonResponse
 from django.shortcuts import render
-
-from wagtail.core.models import Page
 from wagtail.search.models import Query
 
 from .search import cigi_search
 
 
 def search(request):  # pragma: no cover
-    # search_query = request.GET.get('query', None)
-    # page = request.GET.get('page', 1)
-    #
-    # # Search
-    # if search_query:
-    #     search_results = cigi_search(
-    #         searchtext=search_query,
-    #     )
-    #     query = Query.get(search_query)
-    #
-    #     # Record hit
-    #     query.add_hit()
-    # else:
-    #     search_results = Page.objects.none()
-    #
-    # # Pagination
-    # paginator = Paginator(search_results, 10)
-    # try:
-    #     search_results = paginator.page(page)
-    # except PageNotAnInteger:
-    #     search_results = paginator.page(1)
-    # except EmptyPage:
-    #     search_results = paginator.page(paginator.num_pages)
-
-    return render(request, 'search/search.html', {
-        # 'search_query': search_query,
-        # 'search_results': search_results,
-    })
+    return render(request, 'search/search.html')
 
 
 def search_api(request):
+    searchtext = request.GET.get('searchtext', None)
+    if searchtext:
+        query = Query.get(searchtext)
+
+        # Record hit
+        query.add_hit()
+
     pages = cigi_search(
         articletypeid=request.GET.get('articletypeid', None),
         authors=request.GET.getlist('author', None),
@@ -47,7 +24,7 @@ def search_api(request):
         contentsubtypes=request.GET.getlist('contentsubtype', None),
         projects=request.GET.getlist('project', None),
         publicationtypeid=request.GET.get('publicationtypeid', None),
-        searchtext=request.GET.get('searchtext', None),
+        searchtext=searchtext,
         topics=request.GET.getlist('topic', None),
     )
     default_limit = 24
