7b6b2edeeb34be8a8d15411d531b7691762d349c
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-02-24 15:09:45 -0500

    menu items

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -7,4 +7,7 @@ register = template.Library()
 
 @register.simple_tag()
 def get_menu_items(slug):
-    return Menu.objects.get(slug=slug).menu_items.all
+    return Menu.objects.prefetch_related(
+        'menu_items__link_page',
+        'menu_items__submenu__menu_items__link_page',
+    ).get(slug=slug).menu_items.all()
