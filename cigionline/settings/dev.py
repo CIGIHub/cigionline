ead202cc024c49b8fb0df97a2e83e469bbe73f07
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-02-25 14:46:41 -0500

    Enable template fragment caching for header and footer

--- cigionline/settings/dev.py
+++ cigionline/settings/dev.py
@@ -11,13 +11,14 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-INSTALLED_APPS = INSTALLED_APPS + [
-    'debug_toolbar',
-]
-
-MIDDLEWARE = MIDDLEWARE + [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
+# Uncomment this to enable Django debug toolbar
+# INSTALLED_APPS = INSTALLED_APPS + [
+#     'debug_toolbar',
+# ]
+#
+# MIDDLEWARE = MIDDLEWARE + [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
 
 CACHES = {
     'default': {
