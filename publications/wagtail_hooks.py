8a7ebdf692425eb2e73f142bde74df85df715543
johnbxu <johnbxu@gmail.com>
2021-03-01 15:32:25 -0500

    Add publications admin shortcut

---
+++ publications/wagtail_hooks.py
@@ -0,0 +1,22 @@
+from wagtail.core import hooks
+from publications.models import PublicationPage
+from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
+
+
+class PublicationPageModelAdmin(ModelAdmin):
+    # See https://docs.wagtail.io/en/stable/reference/contrib/modeladmin/
+    model = PublicationPage
+    menu_label = 'Publications'
+    menu_icon = 'doc-full'
+    menu_order = 300
+    list_display = ('title', 'publishing_date', 'publication_type', 'live', 'publication_series')
+    list_filter = ('publishing_date', 'publication_type', 'live', 'publication_series')
+    search_fields = ('title')
+    ordering = ['-publishing_date']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(publishing_date__isnull=False)
+
+
+modeladmin_register(PublicationPageModelAdmin)
