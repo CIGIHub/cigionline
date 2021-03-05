d1d3f03ea53f339a51a456ade783b0df019e5185
johnbxu <johnbxu@gmail.com>
2021-03-05 10:07:55 -0500

    Add newsletter admin hook

---
+++ newsletters/wagtail_hooks.py
@@ -0,0 +1,17 @@
+from newsletters.models import NewsletterPage
+from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
+
+
+class NewsletterPageModelAdmin(ModelAdmin):
+    # See https://docs.wagtail.io/en/stable/reference/contrib/modeladmin/
+    model = NewsletterPage
+    menu_label = 'Newsletters'
+    menu_icon = 'site'
+    menu_order = 300
+    list_display = ('title', 'live', 'html_file', 'latest_revision_created_at')
+    list_filter = ('live', 'latest_revision_created_at')
+    search_fields = ('title')
+    ordering = ('-latest_revision_created_at',)
+
+
+modeladmin_register(NewsletterPageModelAdmin)
