10032c53c6721a26bbbd359e50f27f2ce65c900d
johnbxu <johnbxu@gmail.com>
2021-03-10 21:33:35 -0500

    Add swiper imports and markup

--- cigionline/static/pages/annual_report_list_page/index.js
+++ cigionline/static/pages/annual_report_list_page/index.js
@@ -1,7 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+import Swiper, { Navigation, Pagination } from 'swiper';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchTableAnnualReports from '../../js/components/SearchTableAnnualReports';
 import './css/annual_report_list_page.scss';
+import 'swiper/swiper-bundle.css';
+
+Swiper.use([Navigation, Pagination]);
+const swiperContainer = document.querySelector('.swiper-container');
+
+if (swiperContainer) {
+  const annualReportListPageSwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
 
 ReactDOM.render(
   <SearchTableAnnualReports />,
