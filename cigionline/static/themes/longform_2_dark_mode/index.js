1c33af13dcb1e9b47a94d7dc9199d1cab83f71a4
zylene <namscott@gmail.com>
2021-03-14 19:56:39 -0400

    Cigi 456: longform 2 dark (#950)
    * starting
    
    * sticky header
    
    * fixing sticky header and hero
    
    * Revert "fixing sticky header and hero"
    
    This reverts commit 49433fee15b9d69f5e7662232305dc9e72be0bdc.
    
    * clean up js and hero styling
    
    * stream block styling
    
    * moving textbackgroundblock to a stream block
    
    * update template
    
    * text background styling
    
    * link style for paragraphs
    
    * adding blank linke
    
    * fixing lint errors
    
    * add margin
    
    Co-authored-by: Natasha Scott <namscott@soniccat.com>
    Co-authored-by: Jonathan Xu <johnbxu@gmail.com>
    Co-authored-by: Gabriel Tan-Chen <gabriel@tan-chen.com>

--- cigionline/static/themes/longform_2_dark_mode/index.js
+++ cigionline/static/themes/longform_2_dark_mode/index.js
@@ -1,1 +1,42 @@
 import './css/longform_2_dark_mode.scss';
+import ScrollReveal from 'scrollreveal';
+
+let scrolled = null;
+let headerHeight = null;
+let maxHeight = null;
+let scrollTop = 0;
+
+function setScrollPosition() {
+  scrollTop = $(window).scrollTop();
+  scrolled = (scrollTop / maxHeight) * 100;
+  $('progress').attr('value', scrolled);
+
+  if (scrollTop > headerHeight) {
+    $('body').addClass('scrolled');
+  } else {
+    $('body').removeClass('scrolled');
+  }
+}
+
+$(window).on('load', function() {
+  const docHeight = $(document).height();
+  const winHeight = $(window).height();
+  headerHeight = $('header').height();
+
+  maxHeight = docHeight - winHeight;
+  setScrollPosition();
+});
+
+$(window).on('scroll', function() {
+  setScrollPosition();
+});
+
+const sr = ScrollReveal();
+
+sr.reveal('.stream-block-blockquote', {
+  delay: 100,
+  distance: '50px',
+  duration: 750,
+  origin: 'left',
+  scale: 1,
+});
