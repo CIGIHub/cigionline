6f8523fa67babb2e44a88bd55b5bc811afbde1fd
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-06 16:31:08 -0400

    research model admin group

--- research/wagtail_hooks.py
+++ research/wagtail_hooks.py
@@ -1,10 +1,57 @@
+from core.helpers import CIGIModelAdminPermissionHelper
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from wagtail.contrib.modeladmin.helpers import PagePermissionHelper
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
 from wagtail.core import hooks
 
-from .models import TopicPage
+from .models import (
+    ProjectPage,
+    ResearchLandingPage,
+    TopicPage,
+)
+
+
+@hooks.register('register_permissions')
+def register_project_page_permissions():
+    project_content_type = ContentType.objects.get(app_label='research', model='projectpage')
+    return Permission.objects.filter(content_type=project_content_type)
+
+
+class ProjectPageModelAdmin(ModelAdmin):
+    model = ProjectPage
+    menu_label = 'Projects'
+    menu_icon = 'folder'
+    menu_order = 101
+    list_display = ('title', 'publishing_date', 'live')
+    list_filter = ('publishing_date', 'live')
+    search_fields = ('title')
+    ordering = ['-publishing_date']
+    permission_helper_class = CIGIModelAdminPermissionHelper
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(publishing_date__isnull=False)
+
+
+@hooks.register('register_permissions')
+def register_research_landing_page_permissions():
+    research_landing_content_type = ContentType.objects.get(app_label='research', model='researchlandingpage')
+    return Permission.objects.filter(content_type=research_landing_content_type)
+
+
+class ResearchLandingPageModelAdmin(ModelAdmin):
+    model = ResearchLandingPage
+    menu_label = 'Research Landing Page'
+    menu_icon = 'site'
+    menu_order = 100
+    list_display = ('title')
+    search_fields = ('title')
+    ordering = ['title']
+    permission_helper_class = CIGIModelAdminPermissionHelper
 
 
 @hooks.register('register_permissions')
@@ -13,21 +60,23 @@ def register_topic_page_permissions():
     return Permission.objects.filter(content_type=topic_content_type)
 
 
-class TopicPageModelAdminPermissionHelper(PagePermissionHelper):
-    def user_can_list(self, user):
-        return self.user_has_any_permissions(user)
-
-
 class TopicPageModelAdmin(ModelAdmin):
     model = TopicPage
     menu_label = 'Topics'
     menu_icon = 'thumbtack'
-    menu_order = 106
+    menu_order = 102
     list_display = ('title')
     list_filter = ('live')
     search_fields = ('title')
-    order = ['title']
-    permission_helper_class = TopicPageModelAdminPermissionHelper
+    ordering = ['title']
+    permission_helper_class = CIGIModelAdminPermissionHelper
 
 
-modeladmin_register(TopicPageModelAdmin)
+class ResearchModelAdminGroup(ModelAdminGroup):
+    menu_label = 'Research'
+    menu_icon = 'site'
+    menu_order = 106
+    items = (ResearchLandingPageModelAdmin, ProjectPageModelAdmin, TopicPageModelAdmin)
+
+
+modeladmin_register(ResearchModelAdminGroup)
