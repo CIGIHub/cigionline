53fae237e94c058ae73cdf1ee630c24a7f19ad79
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-07 14:23:59 -0400

    people model admin group

--- people/wagtail_hooks.py
+++ people/wagtail_hooks.py
@@ -1,10 +1,31 @@
 from core.helpers import CIGIModelAdminPermissionHelper
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
 from wagtail.core import hooks
 
-from .models import PersonPage
+from .models import PersonListPage, PersonPage
+
+
+@hooks.register('register_permissions')
+def register_person_list_page_permissions():
+    person_list_page_content_type = ContentType.objects.get(app_label='people', model='personlistpage')
+    return Permission.objects.filter(content_type=person_list_page_content_type)
+
+
+class PersonListPageModelAdmin(ModelAdmin):
+    model = PersonListPage
+    menu_label = 'Staff/Expert Landing Pages'
+    menu_icon = 'home'
+    menu_order = 100
+    list_display = ('title',)
+    search_fields = ('title',)
+    ordering = ['title']
+    permission_helper_class = CIGIModelAdminPermissionHelper
 
 
 @hooks.register('register_permissions')
@@ -17,7 +38,7 @@ class PersonPageModelAdmin(ModelAdmin):
     model = PersonPage
     menu_label = 'Staff/Experts'
     menu_icon = 'group'
-    menu_order = 105
+    menu_order = 101
     list_display = ('title', 'position', 'archive', 'live')
     list_filter = ('person_types', 'archive', 'live')
     search_fields = ('title',)
@@ -25,4 +46,11 @@ class PersonPageModelAdmin(ModelAdmin):
     permission_helper_class = CIGIModelAdminPermissionHelper
 
 
-modeladmin_register(PersonPageModelAdmin)
+class PersonModelAdminGroup(ModelAdminGroup):
+    menu_label = 'Staff/Experts'
+    menu_icon = 'group'
+    menu_order = 105
+    items = (PersonListPageModelAdmin, PersonPageModelAdmin)
+
+
+modeladmin_register(PersonModelAdminGroup)
