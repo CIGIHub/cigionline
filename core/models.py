f1bf5289b8820a9ae23bccfa0b9aa6c93216ef37
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-23 09:54:38 -0400

    Add database index for publishing date

--- core/models.py
+++ core/models.py
@@ -402,7 +402,7 @@ class ContentPage(Page, SearchablePageAbstract):
             eventpage__isnull=True
         ).exclude(id__in=exclude_ids).exclude(
             articlepage__article_type__title='CIGI in the News'
-        ).prefetch_related('authors__author', 'topics').order_by('-publishing_date', 'topics')[:12 - len(recommended_content)])
+        ).prefetch_related('authors__author', 'topics').order_by('-publishing_date')[:12 - len(recommended_content)])
 
         recommended_content = list(recommended_content) + additional_content
 
@@ -455,6 +455,11 @@ class ContentPage(Page, SearchablePageAbstract):
         self.bound_field.label = heading
         self.help_text = help_text
         self.bound_field.help_text = help_text
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['publishing_date'])
+        ]
 
 
 class ContentPageAuthor(Orderable):
