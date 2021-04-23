0f5f72e32b1fce5541ca50d174c67edd8ed72d8a
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-23 15:53:28 -0400

    debug toolbar

--- cigionline/settings/production.py
+++ cigionline/settings/production.py
@@ -107,3 +107,30 @@ if 'MAILCHIMP_DATA_CENTER' in os.environ:
     MAILCHIMP_DATA_CENTER = os.environ['MAILCHIMP_DATA_CENTER']
 if 'MAILCHIMP_NEWSLETTER_LIST_ID' in os.environ:
     MAILCHIMP_NEWSLETTER_LIST_ID = os.environ['MAILCHIMP_NEWSLETTER_LIST_ID']
+
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar',
+    'template_profiler_panel',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+    'template_profiler_panel.panels.template.TemplateProfilerPanel',
+]
