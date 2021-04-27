3ae8826117ea588a9017328c71307a3014893111
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-27 16:14:22 -0400

    Generate article image renditions

--- core/management/commands/generate_renditions.py
+++ core/management/commands/generate_renditions.py
@@ -1,13 +1,15 @@
+from articles.models import ArticlePage
 from django.core.management.base import BaseCommand
-from wagtail.images import get_image_model
+from pathlib import Path
+# from wagtail.images import get_image_model
 
 RENDITIONS = [
-    'fill-1600x900',
+    'fill-1600x900',  # og
     'original',
     'width-300',
     'fill-100x100',
     'fill-377x246',
-    'width-1760',
+    'width-1760',  # hero - article
     'width-640',
     'width-700',
     'fill-600x238',
@@ -37,12 +39,40 @@ RENDITIONS = [
 class Command(BaseCommand):
 
     def handle(self, **options):
-        Image = get_image_model()
-        images = Image.objects.all()
-        print(f"Generating renditions for {images.count()} images...")
-        for image in images:
-            for rendition in RENDITIONS:
-                try:
-                    image.get_rendition(rendition)
-                except Exception:
-                    pass
+        batch_limit = 50
+        article_count = ArticlePage.objects.count()
+        print(f'Generating image renditions for {article_count} articles')
+        for i in range((article_count // batch_limit) + 1):
+            articles = ArticlePage.objects.all().order_by('id')[i * 50:(i * 50) + 50]
+            for article in articles:
+                print(f'Article {article.id}: Starting')
+                if article.image_social:
+                    print(f'Article: {article.id}: Generating og image from image_social')
+                    article.image_social.get_rendition('fill-1600x900')
+                elif article.image_hero and Path(article.image_hero.file.url) != '.gif':
+                    print(f'Article: {article.id}: Generating og image from image_hero')
+                    article.image_hero.get_rendition('fill-1600x900')
+                if article.image_hero and Path(article.image_hero.file.url) != '.gif':
+                    print(f'Article: {article.id}: Generating hero image')
+                    article.image_hero.get_rendition('width-1760')
+                if article.image_feature and Path(article.image_feature.file.url) != '.gif':
+                    print(f'Article: {article.id}: Generating medium feature image from image_feature')
+                    article.image_feature.get_rendition('fill-520x390')
+                    print(f'Article: {article.id}: Generating large feature image from image_feature')
+                    article.image_feature.get_rendition('fill-1440x990')
+                elif article.image_hero and Path(article.image_hero.file.url) != '.gif':
+                    print(f'Article: {article.id}: Generating medium feature image from image_hero')
+                    article.image_hero.get_rendition('fill-520x390')
+                    print(f'Article: {article.id}: Generating large feature image from image_hero')
+                    article.image_hero.get_rendition('fill-1440x990')
+                print(f'Article {article.id}: Finished')
+
+        # Image = get_image_model()
+        # images = Image.objects.all()
+        # print(f"Generating renditions for {images.count()} images...")
+        # for image in images:
+        #     for rendition in RENDITIONS:
+        #         try:
+        #             image.get_rendition(rendition)
+        #         except Exception:
+        #             pass
