9e6a61e9d70cb33e2189a135d3a58f1b1ec0543c
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-04-28 07:44:23 -0400

    Clean up

--- core/management/commands/generate_renditions.py
+++ core/management/commands/generate_renditions.py
@@ -7,38 +7,6 @@ from people.models import PersonPage
 from publications.models import PublicationPage
 from pathlib import Path
 
-RENDITIONS = [
-    'fill-1600x900',  # og
-    'original',
-    'width-300',
-    'fill-100x100',
-    'fill-377x246',
-    'width-1760',  # hero - article
-    'width-640',
-    'width-700',
-    'fill-600x238',
-    'fill-672x895',
-    'width-100',
-    'width-600',
-    # These rendition types are used in 2 or fewer templates
-    # skipped to speed up the process
-    # 'fill-1440x990',
-    # 'width-1280',
-    # 'width-1440',
-    # 'width-500',
-    # 'fill-140x140',
-    # 'fill-150x150',
-    # 'fill-200x200',
-    # 'fill-520x390',
-    # 'fill-600x600',
-    # 'fill-600x900',
-    # 'max-450x200',
-    # 'width-1024',
-    # 'width-1920',
-    # 'width-200',
-    # 'width-768',
-]
-
 
 class Command(BaseCommand):
 
