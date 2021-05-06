f1a2715abd209c8a1e4bdf333d9b0db54ba277a0
Sævar Öfjörð Magnússon <saevar@overcast.is>
2021-05-06 15:35:12 +0000

    Fix annual report

--- annual_reports/models.py
+++ annual_reports/models.py
@@ -49,7 +49,7 @@ class AnnualReportListPage(BasicPageAbstract, Page):
     settings_panels = Page.settings_panels + [
         BasicPageAbstract.submenu_panel,
     ]
-    search_fields = Page.search_fields + BasicPageAbstract.search_fields + SearchablePageAbstract.search_fields
+    search_fields = Page.search_fields + BasicPageAbstract.search_fields
 
     class Meta:
         verbose_name = 'Annual Report List Page'
