d8c44725ad58578f6fb8d6eadcc36ea019da4309
Sævar Öfjörð Magnússon <saevar@overcast.is>
2021-05-06 15:31:47 +0000

    Proposed change to promoted search results

--- search/views.py
+++ search/views.py
@@ -2,7 +2,44 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from wagtail.search.models import Query
 
-from .search import cigi_search
+from .search import cigi_search, cigi_search_promoted
+
+
+def process_item(page, request):
+    fields = request.GET.getlist('field', [])
+
+    item = {
+        'highlights': page._highlights,
+        'elevated': page._elevated,
+        'id': page.id,
+        'title': page.title,
+        'url': page.get_url(request),
+    }
+    for field in fields:
+        try:
+            if field == 'authors':
+                item['authors'] = [{
+                    'id': author.author.id,
+                    'title': author.author.title,
+                    'url': author.author.url,
+                } for author in page.specific.authors.all()]
+            elif field == 'cigi_people_mentioned':
+                item['cigi_people_mentioned'] = [{
+                    'id': person.value.id,
+                    'title': person.value.title,
+                    'url': person.value.url,
+                } for person in page.specific.cigi_people_mentioned]
+            elif field == 'topics':
+                item['topics'] = [{
+                    'id': topic.id,
+                    'title': topic.title,
+                    'url': topic.url,
+                } for topic in page.specific.topics.all()]
+            else:
+                item[field] = getattr(page.specific, field)
+        except AttributeError:
+            pass
+    return item
 
 
 def search(request):  # pragma: no cover
@@ -38,6 +75,21 @@ def search_api(request):
         sort=request.GET.get('sort', None),
         topics=request.GET.getlist('topic', None),
     )
+    promoted_pages = cigi_search_promoted(
+        articletypeid=request.GET.get('articletypeid', None),
+        authors=request.GET.getlist('author', None),
+        content_type=request.GET.get('content_type', None),
+        contenttypes=request.GET.getlist('contenttype', None),
+        contentsubtypes=request.GET.getlist('contentsubtype', None),
+        multimediaseriesid=request.GET.get('multimediaseriesid', None),
+        projects=request.GET.getlist('project', None),
+        publicationseriesid=request.GET.get('publicationseriesid', None),
+        publicationtypeid=request.GET.get('publicationtypeid', None),
+        searchtext=searchtext,
+        sort=request.GET.get('sort', None),
+        topics=request.GET.getlist('topic', None),
+    )
+
     default_limit = 24
     default_offset = 0
     limit = request.GET.get('limit', default_limit)
@@ -52,42 +104,11 @@ def search_api(request):
         offset = default_offset
     offsetLimit = limit + offset
 
-    fields = request.GET.getlist('field', [])
-
     items = []
+    for page in promoted_pages:
+        items.append(process_item(page, request))
     for page in pages[offset:offsetLimit]:
-        item = {
-            'highlights': page._highlights,
-            'elevated': page._elevated,
-            'id': page.id,
-            'title': page.title,
-            'url': page.get_url(request),
-        }
-        for field in fields:
-            try:
-                if field == 'authors':
-                    item['authors'] = [{
-                        'id': author.author.id,
-                        'title': author.author.title,
-                        'url': author.author.url,
-                    } for author in page.specific.authors.all()]
-                elif field == 'cigi_people_mentioned':
-                    item['cigi_people_mentioned'] = [{
-                        'id': person.value.id,
-                        'title': person.value.title,
-                        'url': person.value.url,
-                    } for person in page.specific.cigi_people_mentioned]
-                elif field == 'topics':
-                    item['topics'] = [{
-                        'id': topic.id,
-                        'title': topic.title,
-                        'url': topic.url,
-                    } for topic in page.specific.topics.all()]
-                else:
-                    item[field] = getattr(page.specific, field)
-            except AttributeError:
-                pass
-        items.append(item)
+        items.append(process_item(page, request))
 
     return JsonResponse({
         'meta': {
