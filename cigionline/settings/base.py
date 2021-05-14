515edf45d864aa8f84f7f0f34e847940b15a4074
Jonathan Xu <johnbxu@gmail.com>
2021-05-14 13:09:24 -0400

    Update admin base_url (#1442)

--- cigionline/settings/base.py
+++ cigionline/settings/base.py
@@ -278,7 +278,7 @@ WAGTAILAPI_LIMIT_MAX = 40
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'https://edit.cigionline.org'
 
 WAGTAIL_USER_TIME_ZONES = ['America/Toronto']
 TIME_ZONE = 'America/Toronto'
