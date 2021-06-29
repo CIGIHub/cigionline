b2ccc75149d7323cf26a14f4dec58d15a391f398
johnbxu <johnbxu@gmail.com>
2021-06-28 23:26:17 -0400

    Add share buttons, slide 1 opacity and video buttons

--- cigionline/static/js/components/TwentiethPageSlide1Content.js
+++ cigionline/static/js/components/TwentiethPageSlide1Content.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
 const TwentiethPageSlide2Content = ({ slide }) => {
+  const [showBg, setShowBg] = useState(false);
+  const [showShare, setShowShare] = useState(false);
+  const socialString = JSON.parse(
+    document.getElementById('social-string').textContent
+  );
+  const absoluteUrl = JSON.parse(
+    document.getElementById('absolute-url').textContent
+  );
   function playVideo() {
     const videoBg = document.getElementById('video-bg');
     videoBg.play();
@@ -12,6 +20,8 @@ const TwentiethPageSlide2Content = ({ slide }) => {
     const videoBg = document.getElementById('video-bg');
     videoBg.pause();
   }
+
+  function handleBgToggle() {}
 
   return (
     <>
@@ -28,7 +38,7 @@ const TwentiethPageSlide2Content = ({ slide }) => {
           type="video/mp4"
         />
       </video>
-      <div className="opacity-gradient" />
+      <div className={`opacity-gradient ${showBg ? 'show-bg' : ''}`} />
       <div className="slide-1-buttons">
         <div className="container">
           <div className="row justify-content-center text-center">
@@ -42,10 +52,50 @@ const TwentiethPageSlide2Content = ({ slide }) => {
                 </button>
               </div>
               <div className="bg-controls-buttons-right">
-                <button type="button" className="pause">
-                  <i className="fas fa-share-alt" />
+                <ul
+                  className={`social-share-list ${showShare ? 'open' : ''}`}
+                  id="social-share-list-bottom"
+                >
+                  <li className="facebook">
+                    <a className="facebook-share-link" data-url={absoluteUrl}>
+                      <i className="fab fa-facebook-f fa-sm icon" />
+                    </a>
+                  </li>
+                  <li className="twitter">
+                    <a
+                      className="twitter-share-link"
+                      href={`https://twitter.com/share?text=${socialString}&amp;url=${absoluteUrl}`}
+                    >
+                      <i className="fab fa-twitter fa-sm icon" />
+                    </a>
+                  </li>
+                  <li className="linkedin">
+                    <a
+                      className="linkedin-share-link"
+                      href={`https://www.linkedin.com/shareArticle?mini=true&amp;url=${absoluteUrl}&amp;title=${socialString}`}
+                    >
+                      <i className="fab fa-linkedin-in fa-sm icon" />
+                    </a>
+                  </li>
+                </ul>
+                <button
+                  type="button"
+                  className="share"
+                  onClick={() => setShowShare(!showShare)}
+                >
+                  <span className={!showShare ? 'icon-opened' : 'icon-closed'}>
+                    <i className="far fa-arrow-right" />
+                  </span>
+                  <span className={showShare ? 'icon-opened' : 'icon-closed'}>
+                    <i className="fas fa-share-alt" />
+                  </span>
                 </button>
-                <button type="button" className="play">
+                <button
+                  type="button"
+                  className="camera"
+                  onMouseEnter={() => setShowBg(true)}
+                  onMouseLeave={() => setShowBg(false)}
+                >
                   <i className="fas fa-camera" />
                 </button>
               </div>
