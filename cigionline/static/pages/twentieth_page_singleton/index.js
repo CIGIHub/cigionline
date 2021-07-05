25fe9d4c32916cc2d6bb56b6910c0932d97419f3
johnbxu <johnbxu@gmail.com>
2021-07-05 14:31:19 -0400

    Add swiper script

--- cigionline/static/pages/twentieth_page_singleton/index.js
+++ cigionline/static/pages/twentieth_page_singleton/index.js
@@ -4,30 +4,46 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 const swiperContainer = document.querySelector('.swiper-container');
-
 if (swiperContainer) {
   const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 0,
     centeredSlides: true,
-    direction: 'horizontal',
-    watchSlidesProgress: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+  });
+
+  const timelineNavButtons = document.querySelectorAll('#timeline-nav li button');
+  const years = JSON.parse(document.getElementById('years').textContent);
+
+  const toggleYear = function() {
+    [].map.call(timelineNavButtons, function(elem) {
+      elem.classList.remove('active');
+    });
+    this.classList.add('active');
+    swiper.slideTo(years.indexOf(this.dataset.year));
+  };
+  [].map.call(timelineNavButtons, function(elem) {
+    elem.addEventListener('click', toggleYear, false);
+  });
+
+  swiper.on('activeIndexChange', function(e) {
+    for (let i = 0; i < timelineNavButtons.length; i++) {
+      if (timelineNavButtons[i].dataset.year === years[e.activeIndex]) {
+        [].map.call(timelineNavButtons, function(elem) {
+          elem.classList.remove('active');
+        });
+        timelineNavButtons[i].classList.add('active');
+      }
+    }
+
+    swiperContainer.style.paddingBottom = `${60 + swiper.slides[e.activeIndex].querySelector('.gallery-text').offsetHeight}px`;
   });
 }
