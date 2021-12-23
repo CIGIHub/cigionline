ad2eda4b1a3a7c4cd048a0a16929b0e52378e13c
yang-wang-ds <yangwang7890@gmail.com>
2021-12-22 19:47:28 -0500

    add css and template for pfpc series

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,7 @@ const config = {
     themeLongform: './cigionline/static/themes/longform/index.js',
     themeLongform2: './cigionline/static/themes/longform_2/index.js',
     themeLongform2DarkMode: './cigionline/static/themes/longform_2_dark_mode/index.js',
+    themePfPCSeries: './cigionline/static/themes/pfpc_series/index.js',
     themePlatformGovernanceSeries: './cigionline/static/themes/platform_governance_series/index.js',
     themeWomenAndTradeSeries: './cigionline/static/themes/women_and_trade_series/index.js',
     themeWTOSeries: './cigionline/static/themes/wto_series/index.js',
