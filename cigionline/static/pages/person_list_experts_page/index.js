a6fd0a64c26fce8b3074128523fc30152a5eb670
Jonathan Xu <johnbxu@gmail.com>
2022-02-24 15:33:47 -0500

    CIGI-613: Use latest revision for all experts api call cache (#2083)
    * Use latest revision for all experts api call
    
    * Use query param for cache busting
    
    * linter

--- cigionline/static/pages/person_list_experts_page/index.js
+++ cigionline/static/pages/person_list_experts_page/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import SearchTableExperts from '../../js/components/SearchTableExperts';
 import './css/person_list_experts_page.scss';
 
+const revisionDate = document.getElementById('experts-search-table').dataset.revisionDate;
+
 ReactDOM.render(
-  <SearchTableExperts />,
+  <SearchTableExperts revisionDate={revisionDate} />,
   document.getElementById('experts-search-table'),
 );
