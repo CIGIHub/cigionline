140f05a7187fc47efada269e66658ba8ff8a9073
johnbxu <johnbxu@gmail.com>
2022-02-24 15:35:08 -0500

    Add credits

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,6 @@ from django.http import JsonResponse
 def ai_ethics(request):
     series_page = ArticleSeriesPage.objects.get(slug='the-ethics-of-automated-warfare-and-artificial-intelligence')
     introduction_page = series_page.series_items.first().content_page.specific
-    print(introduction_page)
 
     return JsonResponse({
         'title': introduction_page.title,
