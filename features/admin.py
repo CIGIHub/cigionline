e3974d3d022a22fd5bb3fc5e7455a07be9fb476e
johnbxu <johnbxu@gmail.com>
2022-04-27 12:37:02 -0400

    Add admin

--- features/admin.py
+++ features/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
+from .models import HomePageFeaturedPromotionsPage
 
-# Register your models here.
+
+class HomePageFeaturedPromotionsPageModelAdmin(ModelAdmin):
+    model = HomePageFeaturedPromotionsPage
+    menu_label = 'Home Page Featured Promotions'
+    menu_icon = 'image'
+    menu_order = 204
+
+
+class FeaturesModelAdminGroup(ModelAdminGroup):
+    menu_label = 'Features'
+    menu_icon = 'folder-open-inverse'
+    menu_order = 200
+    items = (
+        HomePageFeaturedPromotionsPageModelAdmin,
+    )
+
+
+modeladmin_register(FeaturesModelAdminGroup)
