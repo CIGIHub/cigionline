6145c6698dae0a5e68d1bf3da0f61332bf1dc647
Jonathan Xu <johnbxu@gmail.com>
2022-04-28 14:34:39 -0400

    CIGI-730: Implement separate queue for promotion blocks (#2263)
    * clear cache on save
    
    * delete home page promotion blocks
    
    * Refactor and add fallback option for promotion blocks
    
    * Separate different home page featured sections for chacing
    
    * Add admin
    
    * fix tests

---
+++ features/models.py
@@ -0,0 +1,63 @@
+from django.core.cache import cache
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    InlinePanel,
+    MultiFieldPanel,
+    FieldPanel
+)
+from wagtail.core.models import Page
+from wagtail.core.models import Orderable
+
+
+class FeaturesListPage(Page):
+    parent_page_types = ['home.HomePage']
+    subpage_types = ['features.HomePageFeaturedPromotionsPage']
+    max_count = 1
+
+
+class HomePageFeaturedPromotionsPage(Page):
+    def save(self, *args, **kwargs):
+        cache.delete_many(cache.keys("*homepage_featured_promotions*"))
+        return super().save(*args, **kwargs)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                InlinePanel(
+                    'promotion_blocks',
+                    max_num=4,
+                    min_num=0,
+                    label='Promotion Block',
+                ),
+            ],
+            heading='Promotion Blocks',
+            classname='collapsible',
+        ),
+    ]
+
+    parent_page_types = ['features.FeaturesListPage']
+    subpage_types = []
+    max_count = 1
+
+
+class FeaturesPagePromotionBlocks(Orderable):
+    features_page = ParentalKey(
+        'features.HomePageFeaturedPromotionsPage',
+        related_name='promotion_blocks',
+    )
+    promotion_block = models.ForeignKey(
+        'promotions.PromotionBlock',
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='+',
+        verbose_name='Promotion Block',
+    )
+
+    panels = [
+        FieldPanel(
+            'promotion_block',
+            ['promotions.PromotionBlock'],
+        ),
+    ]
