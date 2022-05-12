eeb64aa4bd8826b31fb31e49328ceda0d30ce756
johnbxu <johnbxu@gmail.com>
2022-05-12 13:58:28 -0400

    Create parallax

--- cigionline/static/themes/four_domains_series/index.js
+++ cigionline/static/themes/four_domains_series/index.js
@@ -1,24 +1,32 @@
 import './css/four_domains_series.scss';
-import ScrollMagic from 'scrollmagic';
 
 const backgroundImages = document.getElementById('background-images');
-const leftImage = backgroundImages.querySelector('.left');
-const rightImage = backgroundImages.querySelector('.right');
+const topImage = backgroundImages.querySelector('.top');
+const midImage = backgroundImages.querySelector('.mid');
+const topImageInitialTop = parseInt(topImage.offsetTop, 10);
+const midImageInitialTop = parseInt(midImage.offsetTop, 10);
+console.log(topImageInitialTop);
 
-let leftOriginalWidth = parseInt(leftImage.offsetWidth, 10);
-let rightOriginalWidth = parseInt(rightImage.offsetWidth, 10);
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
 function animateImage(scrollPos) {
-  leftImage.style.width = `${leftOriginalWidth + scrollPos * 0.2}px`;
-  rightImage.style.width = `${rightOriginalWidth + scrollPos * 0.2}px`;
-  console.log('width: ', Number(scrollPos) * 0.5);
+  if (lastKnownScrollPosition < 800) {
+    topImage.style.top = `${topImageInitialTop - scrollPos * 0.4}px`;
+    midImage.style.top = `${midImageInitialTop - scrollPos * 0.2}px`;
+
+    // topImage.style.transform = `scale(${1 + scrollPos * 0.0002})`;
+    // midImage.style.transform = `scale(${1 + scrollPos * 0.0002})`;
+  } else {
+
+  }
+  // leftImage.style.transform = `scale(${1 + scrollPos * 0.001})`;
+  // rightImage.style.transform = `scale(${1 + scrollPos * 0.001})`;
 }
 
 document.addEventListener('scroll', function (e) {
   lastKnownScrollPosition = window.scrollY;
-  console.log('scrollY: ', lastKnownScrollPosition);
+  console.log(lastKnownScrollPosition);
 
   if (!ticking) {
     window.requestAnimationFrame(function () {
