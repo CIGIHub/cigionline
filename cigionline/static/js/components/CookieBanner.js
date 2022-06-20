1459ea6f3d4cdce78a6ac4408b9b7efe5f6f61af
johnbxu <johnbxu@gmail.com>
2022-06-20 14:48:39 -0400

    Hide banner after click

--- cigionline/static/js/components/CookieBanner.js
+++ cigionline/static/js/components/CookieBanner.js
@@ -8,13 +8,16 @@ class CookieBanner extends React.Component {
       bannerClicked: false,
       bannerComplete: false,
       bannerFade: false,
+      bannerHidden: false,
     };
 
     this.handleBanner = this.handleBanner.bind(this);
   }
 
   handleBanner() {
-    document.cookie = `cigionline.accept.banner=1; path=/; expires=${new Date(2147483647 * 1000).toUTCString()}`;
+    document.cookie = `cigionline.accept.banner=1; path=/; expires=${new Date(
+      2147483647 * 1000,
+    ).toUTCString()}`;
     this.setState({
       bannerClicked: true,
     });
@@ -27,32 +30,57 @@ class CookieBanner extends React.Component {
       this.setState({
         bannerComplete: true,
       });
-    }, 1500);
+    }, 1700);
+    setTimeout(() => {
+      this.setState({
+        bannerHidden: true,
+      });
+    }, 2000);
   }
 
   render() {
-    const { bannerClicked, bannerFade, bannerComplete } = this.state;
-
-    const bannerCopy = `
-      Win one of three $100 Visa cards by participating in CIGI's feedback survey - take the survey, click <a href="https://google.ca">here</a>.
-    `;
+    const {
+      bannerClicked, bannerFade, bannerComplete, bannerHidden,
+    } = this.state;
 
     /* eslint-disable react/no-danger */
     return (
-      <div className={['cigi-cookie-banner', bannerFade && 'banner-fade', bannerComplete && 'banner-complete'].join(' ')}>
+      <div
+        className={[
+          'cigi-cookie-banner',
+          bannerFade && 'banner-fade',
+          bannerComplete && 'banner-complete',
+          bannerHidden && 'banner-hidden',
+        ].join(' ')}
+      >
         <div className="container">
           <div className="row">
             <div className="col-12">
               <div className="cigi-cookie-banner-content">
-                <p className={['cigi-cookie-banner-notice', bannerClicked && 'accepted-banner'].join(' ')}>
-                  Win one of three $100 Visa cards by participating in CIGI&apos;s feedback survey -
-                  take the survey, click&nbsp;
-                  <a href="https://google.ca" target="_blank" rel="noopener noreferrer" onClick={() => this.handleBanner()}>here</a>
+                <p
+                  className={[
+                    'cigi-cookie-banner-notice',
+                    bannerClicked && 'accepted-banner',
+                  ].join(' ')}
+                >
+                  Win one of three $100 Visa cards by participating in
+                  CIGI&apos;s feedback survey - take the survey, click&nbsp;
+                  <a
+                    href="https://google.ca"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => this.handleBanner()}
+                  >
+                    here
+                  </a>
                   .
                 </p>
                 <button
                   type="button"
-                  className={['cigi-cookie-banner-notice', bannerClicked && 'accepted-banner'].join(' ')}
+                  className={[
+                    'cigi-cookie-banner-notice',
+                    bannerClicked && 'accepted-banner',
+                  ].join(' ')}
                   onClick={() => this.handleBanner()}
                 >
                   <i className="fal fa-times" />
