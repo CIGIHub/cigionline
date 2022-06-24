7f3565d18620192a8fac7e14071723b1e1f68c5f
johnbxu <johnbxu@gmail.com>
2022-06-24 08:16:41 -0400

    Add cache clear button

--- caching/views.py
+++ caching/views.py
@@ -1,7 +1,11 @@
-import django_filters
+from email import message
+from tkinter import Widget
+from django_filters import ChoiceFilter
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import gettext_lazy as _
 from django.utils.http import url_has_allowed_host_and_scheme
+from wagtail.admin import messages
 from wagtail.admin.filters import WagtailFilterSet
 from wagtail.admin.views.reports.base import PageReportView
 from wagtail.core.models import Page
@@ -11,8 +15,10 @@ from core.models import Theme
 
 # report filter set that filters based on theme and content series fields
 class CachingReportFilterSet(WagtailFilterSet):
-    theme = django_filters.MultipleChoiceFilter(field_name='theme', choices=Theme.objects.all().values_list('id', 'name'))
-    article_series__title = django_filters.MultipleChoiceFilter(field_name='article_series__title', choices=ArticleSeriesPage.objects.all().values_list('title', 'title'))
+    theme = ChoiceFilter(field_name='theme', choices=Theme.objects.all().values_list('id', 'name'))
+    article_series__title = ChoiceFilter(field_name='article_series__title', choices=ArticleSeriesPage.objects.all().values_list('title', 'title'))
+    article_series__title
+
     class Meta:
         model = Page
         fields = ['theme', 'article_series__title']
@@ -33,9 +39,12 @@ class RecentArticlesView(PageReportView):
 
 def clear_cache(request, page_id):
     page = get_object_or_404(Page, id=page_id).specific
+    keys = cache.keys(f'*Id:{page.id}*')
+    cache.delete(*keys)
+    messages.success(request, _("Cache cleared for page: " + page.title))
+
     redirect_to = request.POST.get('next', None)
-    print(request.POST.get('next'))
-    # if redirect_to and url_has_allowed_host_and_scheme(url=redirect_to, allowed_hosts={request.get_host()}):
-    #     return redirect(redirect_to)
-    # else:
-    #     return redirect('wagtailadmin_explore', page.get_parent().id)
+    if redirect_to and url_has_allowed_host_and_scheme(url=redirect_to, allowed_hosts={request.get_host()}):
+        return redirect(redirect_to)
+    else:
+        return redirect('/admin/caching/')
