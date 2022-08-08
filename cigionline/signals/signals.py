a0a13e06f37fa2b86f3c5662512c8b7b30875ed4
yang-wang-ds <yangwang7890@gmail.com>
2022-08-08 17:11:34 -0400

    replace url

--- cigionline/signals/signals.py
+++ cigionline/signals/signals.py
@@ -1,12 +1,13 @@
 from wagtail.core.signals import page_published
 import requests
 import json
+import os
 
 
 # Let everyone know when a new page is published
 def send_to_slack(sender, **kwargs):
     instance = kwargs['instance']
-    url = 'https://hooks.slack.com/services/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'
+    url = os.environ['SLACK_WEBHOOK_URL']
     values = {
         "text": "%s was published by %s " % (instance.title, instance.owner.username),
     }
