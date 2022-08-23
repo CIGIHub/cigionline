b11e43194bd4c558cbc6857a2baec9ab6ec18fcd
johnbxu <johnbxu@gmail.com>
2022-08-23 09:14:15 -0400

    style series page

--- cigionline/static/themes/space_series/index.js
+++ cigionline/static/themes/space_series/index.js
@@ -1,1 +1,34 @@
 import './css/space_series.scss';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+Swiper.use([Navigation]);
+const swiperContainer = document.querySelector('.swiper-container');
+
+if (swiperContainer) {
+  const spaceSeriesPageSwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      hiddenClass: 'swiper-button-hidden',
+    },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 6,
+      },
+    },
+  });
+}
