0d42f330846d0482b2017975d70a31c627ac87d8
johnbxu <johnbxu@gmail.com>
2022-08-30 11:06:59 -0400

    Update command

--- core/management/commands/update_topics.py
+++ core/management/commands/update_topics.py
@@ -5,16 +5,17 @@ from ...models import ContentPage
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('topic', nargs='+', type=int)
+
     def handle(self, *args, **options):
-        # article_pages = ContentPage.objects.filter(topics__title="Central Banking")
-        # topic_to_add = TopicPage.objects.get(title="Financial Systems")
-        # topic_to_remove = TopicPage.objects.get(title="Central Banking")
+        article_pages = ContentPage.objects.filter(topics__title="Central Banking")
+        print(f'Found {len(article_pages)} pages for topic')
+        topic_to_add = TopicPage.objects.get(title="Financial Systems")
+        topic_to_remove = TopicPage.objects.get(title="Central Banking")
 
-        # for page in article_pages:
-        #     page.topics.remove(topic_to_remove)
-        #     if topic_to_add not in page.topics.all():
-        #         page.topics.add(topic_to_add)
-        #     page.save()
-
-        page = ContentPage.objects.specific.filter(topics__title="IMF", archive=True)
-        print(page)
+        for page in article_pages:
+            page.topics.remove(topic_to_remove)
+            if topic_to_add not in page.topics.all():
+                page.topics.add(topic_to_add)
+            page.save()
