0e06f5095483d45b618318cf988472d519e90296
Jonathan Xu <johnbxu@gmail.com>
2022-10-06 13:06:44 -0400

    CIGI-768: Migrate home page feature sections to separate pages (#2399)
    * Add features models and blocks
    
    * Add cache clear
    
    * Refactor promotion blocks
    
    * Update Home page functions
    
    * Update publications feature list
    
    * resolve conflict and add migration
    
    * Refactor featured publications and events logic
    
    * linter
    
    * Use json_field
    
    * Correct highlights variable

---
+++ features/blocks.py
@@ -0,0 +1,78 @@
+from wagtail.core.blocks import StructBlock, PageChooserBlock
+from wagtail.snippets.blocks import SnippetChooserBlock
+from promotions.models import PromotionBlock
+
+
+class FeaturedPageBlock(StructBlock):
+    page_types = [
+        'publications.PublicationPage',
+        'articles.ArticlePage',
+        'articles.ArticleSeriesPage',
+        'multimedia.MultimediaPage',
+        'multimedia.MultimediaSeriesPage',
+        'events.EventPage',
+        'research.ProjectPage',
+        'annual_reports.AnnualReportPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'pick'
+
+
+class FeaturedPublicationBlock(StructBlock):
+    page_types = [
+        'publications.PublicationPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'pick'
+
+
+class FeaturedExpertBlock(StructBlock):
+    page_types = [
+        'people.PersonPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'person'
+
+
+class FeaturedHighlightBlock(StructBlock):
+    page_types = [
+        'publications.PublicationPage',
+        'articles.ArticleSeriesPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'pick'
+
+
+class FeaturedMultimediaBlock(StructBlock):
+    page_types = [
+        'multimedia.MultimediaPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'play'
+
+
+class FeaturedEventBlock(StructBlock):
+    page_types = [
+        'events.EventPage',
+    ]
+    page = PageChooserBlock(page_type=page_types)
+
+    class Meta:
+        icon = 'calendar'
+
+
+class FeaturedPromotionBlock(StructBlock):
+    block = SnippetChooserBlock(PromotionBlock)
+
+    class Meta:
+        icon = 'pick'
