452542bbc290dc367b7d59b8b0dcc7c8415fd103
johnbxu <johnbxu@gmail.com>
2023-01-31 13:49:43 -0500

    Add styling

--- cigionline/static/pages/multimedia_list_page/index.js
+++ cigionline/static/pages/multimedia_list_page/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Swiper, { Navigation, Pagination } from 'swiper';
 import MultimediaListing from '../../js/components/MultimediaListing';
 import SearchTable from '../../js/components/SearchTable';
+import './css/multimedia_list_page.scss';
+import 'swiper/swiper-bundle.css';
+
+const submenuItems = document.querySelectorAll('.header--submenu__right__menu-item');
+submenuItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    submenuItems.forEach((b) => {
+      b.classList.remove('active');
+    });
+    e.target.classList.add('active');
+  });
+});
+
+Swiper.use([Navigation, Pagination]);
+const swiperContainerFeatured = document.querySelector('.swiper-container--featured');
+const swiperContainerBigTech = document.querySelector('.swiper-container--big-tech');
+
+if (swiperContainerFeatured) {
+  const featuredSlider = new Swiper('.swiper-container--featured', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next-featured',
+      prevEl: '.swiper-button-prev-featured',
+    },
+
+    pagination: {
+      el: '.swiper-pagination-featured',
+      clickable: true,
+    },
+  });
+}
+
+if (swiperContainerBigTech) {
+  const bigTechSlider = new Swiper('.swiper-container--big-tech', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next-big-tech',
+      prevEl: '.swiper-button-prev-big-tech',
+    },
+
+    pagination: {
+      el: '.swiper-pagination-big-tech',
+      clickable: true,
+    },
+  });
+}
 
 ReactDOM.render(
   <SearchTable
@@ -41,6 +100,7 @@ ReactDOM.render(
       'multimedia-list-row',
     ]}
     RowComponent={MultimediaListing}
+    searchPlaceholder="Search Multimedia by Keyword"
   />,
   document.getElementById('multimedia-search-table'),
 );
