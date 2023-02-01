dc1c1109edd6ff40454ab9b289b3406fe04a87d6
johnbxu <johnbxu@gmail.com>
2023-02-01 14:40:47 -0500

    Update menu dropdown icons

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -11,3 +11,16 @@ def get_menu_items(slug):
         'menu_items__link_page',
         'menu_items__submenu__menu_items__link_page',
     ).get(slug=slug).menu_items.all()
+
+@register.filter
+def menu_icon(menu_item):
+    icons = {
+        'Research': 'fa-file-alt',
+        'Publications': 'fa-book',
+        'Experts': 'fa-user',
+        'Opinions': 'fa-comment-alt-lines',
+        'Multimedia': 'fa-film',
+        'Events': 'fa-calendar',
+        'About': 'fa-university',
+    }
+    return icons[menu_item]
