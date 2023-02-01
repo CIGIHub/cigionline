549cf5a407b494ddbbbe8dcdfee60db5d872ce7b
Jonathan Xu <johnbxu@gmail.com>
2023-02-01 11:38:11 -0500

    CIGI-823: Style multimedia landing page (#2563)
    * Initial commit
    
    * Add large mm card template
    
    * Add new featured mm fields
    
    * Add styling
    
    * Add large mm card template
    
    * Add large mm card styling
    
    * Style mm search result cards
    
    * Use wider image for mm search results

--- cigionline/static/pages/multimedia_list_page/index.js
+++ cigionline/static/pages/multimedia_list_page/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Swiper, { Navigation, Pagination } from 'swiper';
 import MultimediaListing from '../../js/components/MultimediaListing';
 import SearchTable from '../../js/components/SearchTable';
+import './css/multimedia_list_page.scss';
+import 'swiper/swiper-bundle.css';
+
+const submenuItems = document.querySelectorAll('.header--submenu__right__menu-item');
+const swipers = {
+  'swiper--featured': document.getElementById('swiper--featured'),
+  'swiper--big-tech': document.getElementById('swiper--big-tech'),
+};
+submenuItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const swiperId = e.target.getAttribute('data-swiper-id');
+    submenuItems.forEach((b) => {
+      const swiperIdInner = b.getAttribute('data-swiper-id');
+      b.classList.remove('active');
+      swipers[swiperIdInner].classList.add('hidden');
+    });
+    e.target.classList.add('active');
+    swipers[swiperId].classList.remove('hidden');
+  });
+});
+
+Swiper.use([Navigation, Pagination]);
+const swiperContainerFeatured = document.querySelector('.swiper-container--featured');
+const swiperContainerBigTech = document.querySelector('.swiper-container--big-tech');
+
+if (swiperContainerFeatured) {
+  const featuredSwiper = new Swiper('.swiper-container--featured', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next-featured',
+      prevEl: '.swiper-button-prev-featured',
+    },
+
+    pagination: {
+      el: '.swiper-pagination-featured',
+      clickable: true,
+    },
+  });
+}
+
+if (swiperContainerBigTech) {
+  const bigTechSwiper = new Swiper('.swiper-container--big-tech', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 800,
+    autoHeight: true,
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next-big-tech',
+      prevEl: '.swiper-button-prev-big-tech',
+    },
+
+    pagination: {
+      el: '.swiper-pagination-big-tech',
+      clickable: true,
+    },
+  });
+}
 
 ReactDOM.render(
   <SearchTable
@@ -14,7 +81,7 @@ ReactDOM.render(
     fields={[
       'authors',
       'contentsubtype',
-      'image_hero_url',
+      'image_hero_wide_url',
       'publishing_date',
       'topics',
     ]}
@@ -37,10 +104,10 @@ ReactDOM.render(
       'row',
       'row-cols-1',
       'row-cols-sm-2',
-      'row-cols-md-3',
       'multimedia-list-row',
     ]}
     RowComponent={MultimediaListing}
+    searchPlaceholder="Search Multimedia by Keyword"
   />,
   document.getElementById('multimedia-search-table'),
 );
