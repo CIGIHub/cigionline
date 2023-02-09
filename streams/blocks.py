36a0a5d61d6e68014584d572ed1aeb2ea2eb08ec
yang-wang-ds <yangwang7890@gmail.com>
2023-02-09 16:00:26 -0500

    customize theme bg colour

--- streams/blocks.py
+++ streams/blocks.py
@@ -1289,7 +1289,7 @@ class ArticleSeriesCard(blocks.StructBlock):
         context['description'] = page.feature_subtitle if page.feature_subtitle else page.short_description
         context['url'] = page.feature_url if page.feature_url else page.url
         context['topics'] = page.topics_sorted
-        # context['']
+        context['theme'] = page.theme
 
         return context
 
