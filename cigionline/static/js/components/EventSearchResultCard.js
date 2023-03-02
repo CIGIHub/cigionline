2eb30134e79e51e0ddbfac1a352a8efb6ce1d58a
yang-wang-ds <yangwang7890@gmail.com>
2023-03-02 13:41:19 -0500

    result cards styling

--- cigionline/static/js/components/EventSearchResultCard.js
+++ cigionline/static/js/components/EventSearchResultCard.js
@@ -28,7 +28,7 @@ const EventSearchResultCard = (props) => {
             {`${startDateMonth} ${startDateDay}`}
           </div>
         )}
-        <h3 className="card__text__title">
+        <h3 className="card--event__title card__text__title">
           <a href={row.url}>{row.title}</a>
         </h3>
         <div className="card--event__info">
