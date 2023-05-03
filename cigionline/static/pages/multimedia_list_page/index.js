e43132d60d8f9f13cd0c3e1583435d8fecfb93ba
johnbxu <johnbxu@gmail.com>
2023-05-03 09:37:22 -0400

    Remove big tech section from mm landing page

--- cigionline/static/pages/multimedia_list_page/index.js
+++ cigionline/static/pages/multimedia_list_page/index.js
@@ -1,75 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Swiper, { Navigation, Pagination } from 'swiper';
 import SearchTableBlockListing from '../../js/components/SearchTableBlockListing';
 import './css/multimedia_list_page.scss';
 import 'swiper/swiper-bundle.css';
 import MultimediaSearchResultCard from '../../js/components/MultimediaSearchResultCard';
 import MultimediaCardLarge from '../../js/components/MultimediaCardLarge';
-
-// const submenuItems = document.querySelectorAll('.header--submenu__right__menu-item');
-// const swipers = {
-//   'swiper--featured': document.getElementById('swiper--featured'),
-//   'swiper--big-tech': document.getElementById('swiper--big-tech'),
-// };
-// submenuItems.forEach((item) => {
-//   item.addEventListener('click', (e) => {
-//     const swiperId = e.target.getAttribute('data-swiper-id');
-//     submenuItems.forEach((b) => {
-//       const swiperIdInner = b.getAttribute('data-swiper-id');
-//       b.classList.remove('active');
-//       swipers[swiperIdInner].classList.add('hidden');
-//     });
-//     e.target.classList.add('active');
-//     swipers[swiperId].classList.remove('hidden');
-//   });
-// });
-
-// Swiper.use([Navigation, Pagination]);
-// const swiperContainerFeatured = document.querySelector('.swiper-container--featured');
-// const swiperContainerBigTech = document.querySelector('.swiper-container--big-tech');
-
-// if (swiperContainerFeatured) {
-//   const featuredSwiper = new Swiper('.swiper-container--featured', {
-//     slidesPerView: 1,
-//     slidesPerGroup: 1,
-//     spaceBetween: 20,
-//     speed: 800,
-//     autoHeight: true,
-//     grabCursor: true,
-
-//     navigation: {
-//       nextEl: '.swiper-button-next-featured',
-//       prevEl: '.swiper-button-prev-featured',
-//     },
-
-//     pagination: {
-//       el: '.swiper-pagination-featured',
-//       clickable: true,
-//     },
-//   });
-// }
-
-// if (swiperContainerBigTech) {
-//   const bigTechSwiper = new Swiper('.swiper-container--big-tech', {
-//     slidesPerView: 1,
-//     slidesPerGroup: 1,
-//     spaceBetween: 20,
-//     speed: 800,
-//     autoHeight: true,
-//     grabCursor: true,
-
-//     navigation: {
-//       nextEl: '.swiper-button-next-big-tech',
-//       prevEl: '.swiper-button-prev-big-tech',
-//     },
-
-//     pagination: {
-//       el: '.swiper-pagination-big-tech',
-//       clickable: true,
-//     },
-//   });
-// }
 
 const featuredPage = JSON.parse(document.getElementById('multimedia-search-table').dataset.featuredPage);
 ReactDOM.render(
