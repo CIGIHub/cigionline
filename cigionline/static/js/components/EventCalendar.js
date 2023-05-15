a774d879e5ff297e20e2d2db7c66e271ab3eec21
johnbxu <johnbxu@gmail.com>
2023-05-15 12:27:30 -0400

    update js

--- cigionline/static/js/components/EventCalendar.js
+++ cigionline/static/js/components/EventCalendar.js
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 import Popover from 'bootstrap/js/dist/popover';
 // import 'bootstrap/dist/js/bootstrap.bundle';
 
+let popoverList = [];
 export default class EventCalendar extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +18,12 @@ export default class EventCalendar extends React.Component {
   componentDidMount() {
     // fetch events for current month.
     this.fetchEvents(new Date());
+    const popoverTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="popover"]',
+    );
+    popoverList = [...popoverTriggerList].map(
+      (popoverTriggerEl) => new Popover(popoverTriggerEl),
+    );
   }
 
   /**
@@ -27,8 +34,14 @@ export default class EventCalendar extends React.Component {
     if (!eventsOnThisDate.length) {
       return null;
     }
-    const borderClass = eventsOnThisDate.length > 4 ? 5 : eventsOnThisDate.length + 1;
-    const eventLinks = eventsOnThisDate.map((eventOnThisDate) => `<a href="${eventOnThisDate.url}">${eventOnThisDate.title}</a>`).join('');
+    const borderClass =
+      eventsOnThisDate.length > 4 ? 5 : eventsOnThisDate.length + 1;
+    const eventLinks = eventsOnThisDate
+      .map(
+        (eventOnThisDate) =>
+          `<a href="${eventOnThisDate.url}">${eventOnThisDate.title}</a>`,
+      )
+      .join('');
     const popOverHtml = `<div class="react-calendar__tile__popover">${eventLinks}</div>`;
     return (
       <div
@@ -49,23 +62,25 @@ export default class EventCalendar extends React.Component {
         data-animation="false"
       />
     );
-  }
+  };
 
   fetchEvents = (date) => {
     this.setState({ events: [], isLoading: true });
-    fetch(`/api/events/?month=${date.getMonth() + 1}&year=${date.getFullYear()}`)
+    fetch(
+      `/api/events/?month=${date.getMonth() + 1}&year=${date.getFullYear()}`,
+    )
       .then((res) => res.json())
       .then((res) => {
         this.setState({ events: res.items, isLoading: false });
       });
-  }
+  };
 
   // Prevent popover from abruptly closing when user tries to hover on the event link.
   showPopover = (e) => {
-    console.log(e.target)
     const popOver = new Popover(e.target);
     popOver.show();
-    e.addeventlistener('mouseleave', () => {
+    console.log(popoverList)
+    e.addEventListener('mouseleave', () => {
       popOver.hide();
     });
     // $(e.target).popover();
@@ -73,21 +88,21 @@ export default class EventCalendar extends React.Component {
     // $('.popover').on('mouseleave', function() {
     //   $(e.target).popover('hide');
     // });
-  }
+  };
 
   hidePopover = (e) => {
-    setTimeout(function() {
+    setTimeout(function () {
       if (!$('.popover:hover').length) {
         $(e.target).popover('hide');
       }
     }, 100);
-  }
+  };
 
   // Toggle popover for mobile devices
   togglePopover = (e) => {
     $(e.target).popover();
     $(e.target).popover('toggle');
-  }
+  };
 
   tileClassName = ({ date }) => {
     const eventsOnThisDate = this.filterEvents(date);
@@ -95,17 +110,18 @@ export default class EventCalendar extends React.Component {
       return 'react-calendar__tile--has-event';
     }
     return null;
-  }
+  };
 
   /**
    * Format weekdays to their initials.
    * E.g. Sunday -> S
    * */
-  formatShortWeekday = (locale, date) => date.toLocaleString(locale, { weekday: 'narrow' })
+  formatShortWeekday = (locale, date) =>
+    date.toLocaleString(locale, { weekday: 'narrow' });
 
   onActiveStartDateChange = ({ activeStartDate }) => {
     this.fetchEvents(activeStartDate);
-  }
+  };
 
   /**
    * Filter event by date
@@ -116,7 +132,10 @@ export default class EventCalendar extends React.Component {
     const { events } = this.state;
     return events.filter((e) => {
       const eventDate = new Date(e.publishing_date);
-      return eventDate.getDate() === date.getDate() && eventDate.getMonth() === date.getMonth();
+      return (
+        eventDate.getDate() === date.getDate() &&
+        eventDate.getMonth() === date.getMonth()
+      );
     });
   }
 
@@ -142,17 +161,15 @@ export default class EventCalendar extends React.Component {
           tileContent={tileContent}
           tileClassName={tileClassName}
         />
-        {isLoading
-          ? (
-            <div className="events-calendar__loader">
-              <img
-                src="/static/assets/loader_spinner.gif"
-                alt="Loading..."
-                className="loading-spinner"
-              />
-            </div>
-          )
-          : null}
+        {isLoading ? (
+          <div className="events-calendar__loader">
+            <img
+              src="/static/assets/loader_spinner.gif"
+              alt="Loading..."
+              className="loading-spinner"
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
