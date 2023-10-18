d38480f001b2579fdde01ac1cb61e61767377f57
yang-wang-ds <yangwang7890@gmail.com>
2023-10-18 16:41:25 -0400

    refactor in prog: class structure and inheritance setup

--- signals/apps.py
+++ signals/apps.py
@@ -12,90 +12,203 @@ from wagtail.contrib.frontend_cache.utils import purge_url_from_cache
 from wagtail.signals import page_published
 
 
-def get_env():
-    # PYTHON_ENV values: 'production', 'admin', 'staging'
-    if 'PYTHON_ENV' in os.environ:
-        return os.environ.get('PYTHON_ENV')
-    return 'dev'
+class InstanceInfo():
+    def __init__(self, instance):
+        self.instance = instance
+        self.title = instance.title
+        self.authors = ', '.join(instance.author_names)
+        self.page_owner = f'{instance.owner.first_name} {instance.owner.last_name}'
+        self.content_type = 'Articles' if instance.contenttype == 'Opinion' else instance.contenttype  # adjust ContentPage.contenttype to match page_type
+        self.publisher = f'{instance.get_latest_revision().user.first_name} {instance.get_latest_revision().user.last_name}'
+        self.relative_url = instance.get_url_parts()[-1]  # last item in the tuple is the relative url to root; e.g. /articles/an-article/
 
 
-def get_site_url():
-    env = get_env()
+class NotificationFlags(InstanceInfo):
+    def __init__(self, instance):
+        super().__init__(instance)
 
-    # values are the host root url intended for users' viewing. admin/production both return live site; staging returns staging site; dev returns local
-    site_url_dict = {
-        'admin': 'https://www.cigionline.org',
-        'production': 'https://www.cigionline.org',
-        'staging': 'https://staging.cigionline.org',
-        'dev': 'http://localhost:8000'
-    }
-    return site_url_dict[env]
+    def _datetime_compare(self, t1, t2):
+        """
+        t1: go_live_at field
+        t2: last_published_at field
 
+        if go_live_at field is populated, compare last_published_at to go_live_at
+        if not, default False
+        """
+        if t1:
+            return ((t2.astimezone(pytz.utc) - t1.astimezone(pytz.utc)) <= datetime.timedelta(hours=1))
+        return False
 
-def datetime_compare(t1, t2):
-    if t1 and t2:  # if go_live_at field is populated; if not, default False
-        return ((t2.astimezone(pytz.utc) - t1.astimezone(pytz.utc)) <= datetime.timedelta(hours=1))
-    return False
+    def _count_publishes(self):
+        from wagtail.models import PageLogEntry
 
+        all_unpublishes = PageLogEntry.objects.filter(page_id=self.instance.id, action='wagtail.unpublish').order_by('-timestamp')
+        if all_unpublishes:
+            latest_unpublish = all_unpublishes[0].timestamp
+            all_publishes = PageLogEntry.objects.filter(page_id=self.instance.id, action='wagtail.publish', timestamp__gt=latest_unpublish)
+        else:
+            all_publishes = PageLogEntry.objects.filter(page_id=self.instance.id, action='wagtail.publish')
 
-def get_publish_trigger_type(instance, revision):
-    if revision.approved_go_live_at:
-        if datetime_compare(instance.go_live_at, instance.last_published_at):
-            return 'scheduled'
-        return 'scheduling'
-    return 'manual'
+        # for some reason, the PageLogEntry objects are not including the most recent publish that triggered this script
+        # so a first-time publish would have a count of 0
+        is_first_publish = (len(all_publishes) == 0)
 
+        if self.instance.go_live_at:
+            publishes_since_go_live_at = PageLogEntry.objects.filter(page_id=self.instance.id, action='wagtail.publish', timestamp__gte=self.instance.go_live_at)
+            is_first_publish_since_go_live_at = (len(publishes_since_go_live_at) == 0)
+        else:
+            is_first_publish_since_go_live_at = False
+        return is_first_publish, is_first_publish_since_go_live_at
 
-def count_publishes(instance):
-    from wagtail.models import PageLogEntry
+    @property
+    def is_first_publish(self):
+        return self._count_publishes()[0]
 
-    all_unpublishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.unpublish').order_by('-timestamp')
-    if all_unpublishes:
-        latest_unpublish = all_unpublishes[0].timestamp
-        all_publishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.publish', timestamp__gt=latest_unpublish)
-    else:
-        all_publishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.publish')
+    @property
+    def is_first_publish_since_go_live_at(self):
+        return self._count_publishes()[1]
 
-    # for some reason, the PageLogEntry objects are not including the most recent publish that triggered this script
-    # so a first-time publish would have a count of 0
-    is_first_publish = (len(all_publishes) == 0)
-
-    return is_first_publish
-
-
-def instance_info(instance):
-    title = instance.title
-    authors = ', '.join(instance.author_names)
-    page_owner = f'{instance.owner.first_name} {instance.owner.last_name}'
-    content_type = 'Articles' if instance.contenttype == 'Opinion' else instance.contenttype  # adjust ContentPage.contenttype to match page_type
-    publisher = f'{instance.get_latest_revision().user.first_name} {instance.get_latest_revision().user.last_name}'
-    is_first_publish = count_publishes(instance)
-    relative_url = instance.get_url_parts()[-1]  # last item in the tuple is the relative url to root; e.g. /articles/an-article/
-    return title, authors, page_owner, content_type, publisher, is_first_publish, relative_url
+    @property
+    def is_scheduled_publish(self):
+        return (self._datetime_compare(self.instance.go_live_at, self.instance.last_published_at) and self.is_first_publish_since_go_live_at)
 
 
-def notification_user_list(content_type, is_first_publish, is_scheduled_publish):
-    from .models import PublishEmailNotification
+class NotificationRecipients(NotificationFlags):
+    """
+    Determine recipients based on flags
+    """
 
-    user_list = PublishEmailNotification.objects.filter(page_type_permissions__page_type__title__contains=content_type)
+    def __init__(self, instance):
+        super().__init__(instance)
 
-    # additional filter based on scanerios created by combinations of the two flags
-    if is_first_publish:
-        user_list = user_list.filter(state_opt_in__in=('first_time', 'both'))
-    else:
-        user_list = user_list.filter(state_opt_in__in=('republish', 'both'))
-    if is_scheduled_publish:
-        user_list = user_list.filter(trigger_opt_in__in=('scheduled', 'both'))
-    else:
-        user_list = user_list.filter(trigger_opt_in__in=('manual', 'both'))
-    return user_list
+    def get_notification_user_list(self):
+        from .models import PublishEmailNotification
+
+        user_list = PublishEmailNotification.objects.filter(page_type_permissions__page_type__title__contains=self.content_type)
+
+        # additional filter based on scanerios created by combinations of the two flags
+        if self.is_first_publish:
+            user_list = user_list.filter(state_opt_in__in=('first_time', 'both'))
+        else:
+            user_list = user_list.filter(state_opt_in__in=('republish', 'both'))
+        if self.is_scheduled_publish:
+            user_list = user_list.filter(trigger_opt_in__in=('scheduled', 'both'))
+        else:
+            user_list = user_list.filter(trigger_opt_in__in=('manual', 'both'))
+        return user_list
+
+    @property
+    def notification_email_list(self):
+        # going through User model because PublishEmailNotification returns UserProfile objects which has no 'email' attribute
+        from django.contrib.auth.models import User
+
+        return [User.objects.get(id=user_to_notify.user.user_id).email for user_to_notify in self.get_notification_user_list()]
 
 
-def notification_email_list(notification_user_list):
-    # going through User model because PublishEmailNotification returns UserProfile objects which has no 'email' attribute
-    from django.contrib.auth.models import User
+class NotificationContent(NotificationRecipients):
 
-    return [User.objects.get(id=user_to_notify.user.user_id).email for user_to_notify in notification_user_list]
+    @property
+    def get_site_url(self):
+        """
+        PYTHON_ENV values: 'production', 'admin', 'staging'
+
+        return values are the host root url intended for users' viewing
+        admin/production both return live site
+        staging returns staging site; dev returns local
+        """
+        env = os.environ.get('PYTHON_ENV') if 'PYTHON_ENV' in os.environ else 'dev'
+
+        site_url_dict = {
+            'admin': 'https://www.cigionline.org',
+            'production': 'https://www.cigionline.org',
+            'staging': 'https://staging.cigionline.org',
+            'dev': 'http://localhost:8000'
+        }
+        return site_url_dict[env]
+
+
+# def get_env():
+#     # PYTHON_ENV values: 'production', 'admin', 'staging'
+#     if 'PYTHON_ENV' in os.environ:
+#         return os.environ.get('PYTHON_ENV')
+#     return 'dev'
+
+
+# def get_site_url():
+#     env = get_env()
+
+#     # values are the host root url intended for users' viewing. admin/production both return live site; staging returns staging site; dev returns local
+#     site_url_dict = {
+#         'admin': 'https://www.cigionline.org',
+#         'production': 'https://www.cigionline.org',
+#         'staging': 'https://staging.cigionline.org',
+#         'dev': 'http://localhost:8000'
+#     }
+#     return site_url_dict[env]
+
+
+# def datetime_compare(t1, t2):
+#     if t1:  # if go_live_at field is populated; if not, default False
+#         return ((t2.astimezone(pytz.utc) - t1.astimezone(pytz.utc)) <= datetime.timedelta(hours=1))
+#     return False
+
+
+# def count_publishes(instance):
+#     from wagtail.models import PageLogEntry
+
+#     all_unpublishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.unpublish').order_by('-timestamp')
+#     if all_unpublishes:
+#         latest_unpublish = all_unpublishes[0].timestamp
+#         all_publishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.publish', timestamp__gt=latest_unpublish)
+#     else:
+#         all_publishes = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.publish')
+
+#     # for some reason, the PageLogEntry objects are not including the most recent publish that triggered this script
+#     # so a first-time publish would have a count of 0
+#     is_first_publish = (len(all_publishes) == 0)
+
+#     if instance.go_live_at:
+#         publishes_since_go_live_at = PageLogEntry.objects.filter(page_id=instance.id, action='wagtail.publish', timestamp__gte=instance.go_live_at)
+#         is_first_publish_since_go_live_at = (len(publishes_since_go_live_at) == 0)
+#     else:
+#         is_first_publish_since_go_live_at = False
+#     return is_first_publish, is_first_publish_since_go_live_at
+
+
+# def instance_info(instance):
+#     title = instance.title
+#     authors = ', '.join(instance.author_names)
+#     page_owner = f'{instance.owner.first_name} {instance.owner.last_name}'
+#     content_type = 'Articles' if instance.contenttype == 'Opinion' else instance.contenttype  # adjust ContentPage.contenttype to match page_type
+#     publisher = f'{instance.get_latest_revision().user.first_name} {instance.get_latest_revision().user.last_name}'
+#     is_first_publish, is_first_publish_since_go_live_at = count_publishes(instance)
+#     is_scheduled_publish = (datetime_compare(instance.go_live_at, instance.last_published_at) and is_first_publish_since_go_live_at)
+#     relative_url = instance.get_url_parts()[-1]  # last item in the tuple is the relative url to root; e.g. /articles/an-article/
+#     return title, authors, page_owner, content_type, publisher, is_first_publish, is_scheduled_publish, relative_url
+
+
+# def notification_user_list(content_type, is_first_publish, is_scheduled_publish):
+#     from .models import PublishEmailNotification
+
+#     user_list = PublishEmailNotification.objects.filter(page_type_permissions__page_type__title__contains=content_type)
+
+#     # additional filter based on scanerios created by combinations of the two flags
+#     if is_first_publish:
+#         user_list = user_list.filter(state_opt_in__in=('first_time', 'both'))
+#     else:
+#         user_list = user_list.filter(state_opt_in__in=('republish', 'both'))
+#     if is_scheduled_publish:
+#         user_list = user_list.filter(trigger_opt_in__in=('scheduled', 'both'))
+#     else:
+#         user_list = user_list.filter(trigger_opt_in__in=('manual', 'both'))
+#     return user_list
+
+
+# def notification_email_list(notification_user_list):
+#     # going through User model because PublishEmailNotification returns UserProfile objects which has no 'email' attribute
+#     from django.contrib.auth.models import User
+
+#     return [User.objects.get(id=user_to_notify.user.user_id).email for user_to_notify in notification_user_list]
 
 
 def notifications_on():
@@ -135,7 +248,8 @@ def send_email(title, authors, page_owner, content_type, recipients, publisher, 
     )
     msg.attach_alternative(html_content, "text/html")
 
-    msg.send()
+    if notifications_on():
+        msg.send()
 
 
 def send_to_slack(title, authors, page_owner, content_type, publisher, publish_phrasing, page_url, header_label):
@@ -152,31 +266,27 @@ def send_to_slack(title, authors, page_owner, content_type, publisher, publish_p
     }
     url = os.environ['SLACK_WEBHOOK_URL']  # hardcoded placeholder test channel
 
-    requests.post(url, json.dumps(values))
+    if notifications_on():
+        requests.post(url, json.dumps(values))
 
 
 # Let everyone know when a new page is published
 def send_notifications(sender, **kwargs):
     instance = kwargs['instance']
-    revision = kwargs['revision']
-    publish_trigger_type = get_publish_trigger_type(instance, revision)
 
-    # first ever scheduled publishes trigger this signal unexpectedly upon scheduling; filter them out
-    if publish_trigger_type != 'scheduling':
-        # wrap in try/except to not disrupt normal operations if a page is successfully published but email could not be sent
-        try:
-            title, authors, page_owner, content_type, publisher, is_first_publish, relative_url = instance_info(instance)
-            is_scheduled_publish = publish_trigger_type == 'scheduled'
-            publish_phrasing = set_publish_phrasing(is_first_publish)
-            page_url = f'{get_site_url()}{relative_url}'
-            header_label = get_header_label()
+    title, authors, page_owner, content_type, publisher, is_first_publish, is_scheduled_publish, relative_url = instance_info(instance)
+    publish_phrasing = set_publish_phrasing(is_first_publish)
+    page_url = f'{get_site_url()}{relative_url}'
+    header_label = get_header_label()
 
-            if is_first_publish:
-                send_to_slack(title, authors, page_owner, content_type, publisher, publish_phrasing, page_url, header_label)
-            notification_list = notification_email_list(notification_user_list(content_type, is_first_publish, is_scheduled_publish))
-            send_email(title, authors, page_owner, content_type, notification_list, publisher, publish_phrasing, page_url, header_label)
-        except Exception as e:
-            print(e)
+    # wrap in try/except to not disrupt normal operations if a page is successfully published but email could not be sent
+    try:
+        if is_first_publish:
+            send_to_slack(title, authors, page_owner, content_type, publisher, publish_phrasing, page_url, header_label)
+        notification_list = notification_email_list(notification_user_list(content_type, is_first_publish, is_scheduled_publish))
+        send_email(title, authors, page_owner, content_type, notification_list, publisher, publish_phrasing, page_url, header_label)
+    except Exception as e:
+        print(e)
 
 
 def clear_cloudflare_home_page_cache(sender, **kwargs):
@@ -188,13 +298,13 @@ def clear_cloudflare_home_page_cache(sender, **kwargs):
 
 def clear_experts_page_cache(sender, **kwargs):
     # clear experts landing page search table cache if a new expert is added or a person gains or loses the 'expert' role, or if an expert's expertise field is updated
-    from wagtail.models import Revision
+    from wagtail.models import PageRevision
 
     revision = kwargs['revision']
 
     try:
         revision_previous = revision.get_previous()
-    except Revision.DoesNotExist:
+    except PageRevision.DoesNotExist:
         revision_previous = None
 
     try:
@@ -228,12 +338,11 @@ class SignalsConfig(AppConfig):
             HomePageFeaturedExpertsList,
         )
 
-        if notifications_on():
-            page_published.connect(send_notifications, sender=ArticlePage)
-            page_published.connect(send_notifications, sender=ArticleSeriesPage)
-            page_published.connect(send_notifications, sender=PublicationPage)
-            page_published.connect(send_notifications, sender=MultimediaPage)
-            page_published.connect(send_notifications, sender=EventPage)
+        page_published.connect(send_notifications, sender=ArticlePage)
+        page_published.connect(send_notifications, sender=ArticleSeriesPage)
+        page_published.connect(send_notifications, sender=PublicationPage)
+        page_published.connect(send_notifications, sender=MultimediaPage)
+        page_published.connect(send_notifications, sender=EventPage)
 
         if 'PYTHON_ENV' in os.environ \
                 and (os.environ.get('PYTHON_ENV') == 'production' or os.environ.get('PYTHON_ENV') == 'admin') \
