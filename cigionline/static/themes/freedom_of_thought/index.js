476a30c29a5a1ab401723ce8dd59ff5f1e2d9f4d
yang-wang-ds <50630672+yang-wang-ds@users.noreply.github.com>
2024-01-15 10:16:00 -0500

    CIGI-933b: freedom of thought project page search table (#2723)
    * add search table
    
    * declare projectId

--- cigionline/static/themes/freedom_of_thought/index.js
+++ cigionline/static/themes/freedom_of_thought/index.js
@@ -1,1 +1,98 @@
+/* global projectId */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectContentListing from '../../js/components/ProjectContentListing';
+import SearchTable from '../../js/components/SearchTable';
 import './css/fot.scss';
+
+const endpointParams = [];
+if (projectId) {
+  endpointParams.push({
+    paramName: 'project',
+    paramValue: projectId,
+  });
+}
+
+ReactDOM.render(
+  <SearchTable
+    showSearch
+    endpointParams={endpointParams}
+    fields={[
+      'authors',
+      'contentsubtype',
+      'contenttype',
+      'pdf_download',
+      'publishing_date',
+      'topics',
+    ]}
+    containerClass={[
+      'custom-theme-table',
+    ]}
+    filterTypes={[{
+      name: 'Event',
+      params: [{
+        name: 'contenttype',
+        value: 'Event',
+      }],
+    }, {
+      name: 'Publication',
+      params: [{
+        name: 'contenttype',
+        value: 'Publication',
+      }],
+    }, {
+      name: 'Multimedia',
+      params: [{
+        name: 'contenttype',
+        value: 'Multimedia',
+      }],
+    }, {
+      name: 'Opinion',
+      aggregationField: 'contentsubtypes',
+      params: [{
+        name: 'contentsubtype',
+        value: 'Opinion',
+      }],
+    }, {
+      name: 'Op-Eds',
+      aggregationField: 'contentsubtypes',
+      params: [{
+        name: 'contentsubtype',
+        value: 'Op-Eds',
+      }],
+    }, {
+      name: 'CIGI in the News',
+      aggregationField: 'contentsubtypes',
+      params: [{
+        name: 'contentsubtype',
+        value: 'CIGI in the News',
+      }],
+    }, {
+      name: 'News Releases',
+      aggregationField: 'contentsubtypes',
+      params: [{
+        name: 'contentsubtype',
+        value: 'News Releases',
+      }],
+    }]}
+    RowComponent={ProjectContentListing}
+    tableColumns={[{
+      colSpan: 4,
+      colTitle: 'Title',
+    }, {
+      colSpan: 3,
+      colTitle: 'Expert',
+    }, {
+      colSpan: 2,
+      colTitle: 'Topic',
+    }, {
+      colSpan: 2,
+      colTitle: 'Type',
+    }, {
+      colSpan: 1,
+      colTitle: 'PDF',
+    }]}
+  />,
+  document.getElementById('project-search-table'),
+);
