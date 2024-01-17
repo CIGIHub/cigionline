ba386e18782388ebfdb795c6352f92786e96b750
johnbxu <johnbxu@gmail.com>
2024-01-17 14:44:53 -0400

    Hide search tables on project pages with no tagged pages

--- cigionline/static/pages/project_page/index.js
+++ cigionline/static/pages/project_page/index.js
@@ -1,4 +1,5 @@
 /* global projectId */
+/* global hasTaggedPages */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProjectContentListing from '../../js/components/ProjectContentListing';
@@ -13,85 +14,87 @@ if (projectId) {
   });
 }
 
-ReactDOM.render(
-  <SearchTable
-    showSearch
-    endpointParams={endpointParams}
-    fields={[
-      'authors',
-      'contentsubtype',
-      'contenttype',
-      'pdf_download',
-      'publishing_date',
-      'topics',
-    ]}
-    containerClass={[
-      'custom-theme-table',
-    ]}
-    filterTypes={[{
-      name: 'Event',
-      params: [{
-        name: 'contenttype',
-        value: 'Event',
-      }],
-    }, {
-      name: 'Publication',
-      params: [{
-        name: 'contenttype',
-        value: 'Publication',
-      }],
-    }, {
-      name: 'Multimedia',
-      params: [{
-        name: 'contenttype',
-        value: 'Multimedia',
-      }],
-    }, {
-      name: 'Opinion',
-      aggregationField: 'contentsubtypes',
-      params: [{
-        name: 'contentsubtype',
-        value: 'Opinion',
-      }],
-    }, {
-      name: 'Op-Eds',
-      aggregationField: 'contentsubtypes',
-      params: [{
-        name: 'contentsubtype',
-        value: 'Op-Eds',
-      }],
-    }, {
-      name: 'CIGI in the News',
-      aggregationField: 'contentsubtypes',
-      params: [{
-        name: 'contentsubtype',
-        value: 'CIGI in the News',
-      }],
-    }, {
-      name: 'News Releases',
-      aggregationField: 'contentsubtypes',
-      params: [{
-        name: 'contentsubtype',
-        value: 'News Releases',
-      }],
-    }]}
-    RowComponent={ProjectContentListing}
-    tableColumns={[{
-      colSpan: 4,
-      colTitle: 'Title',
-    }, {
-      colSpan: 3,
-      colTitle: 'Expert',
-    }, {
-      colSpan: 2,
-      colTitle: 'Topic',
-    }, {
-      colSpan: 2,
-      colTitle: 'Type',
-    }, {
-      colSpan: 1,
-      colTitle: 'PDF',
-    }]}
-  />,
-  document.getElementById('project-search-table'),
-);
+if (hasTaggedPages) {
+  ReactDOM.render(
+    <SearchTable
+      showSearch
+      endpointParams={endpointParams}
+      fields={[
+        'authors',
+        'contentsubtype',
+        'contenttype',
+        'pdf_download',
+        'publishing_date',
+        'topics',
+      ]}
+      containerClass={[
+        'custom-theme-table',
+      ]}
+      filterTypes={[{
+        name: 'Event',
+        params: [{
+          name: 'contenttype',
+          value: 'Event',
+        }],
+      }, {
+        name: 'Publication',
+        params: [{
+          name: 'contenttype',
+          value: 'Publication',
+        }],
+      }, {
+        name: 'Multimedia',
+        params: [{
+          name: 'contenttype',
+          value: 'Multimedia',
+        }],
+      }, {
+        name: 'Opinion',
+        aggregationField: 'contentsubtypes',
+        params: [{
+          name: 'contentsubtype',
+          value: 'Opinion',
+        }],
+      }, {
+        name: 'Op-Eds',
+        aggregationField: 'contentsubtypes',
+        params: [{
+          name: 'contentsubtype',
+          value: 'Op-Eds',
+        }],
+      }, {
+        name: 'CIGI in the News',
+        aggregationField: 'contentsubtypes',
+        params: [{
+          name: 'contentsubtype',
+          value: 'CIGI in the News',
+        }],
+      }, {
+        name: 'News Releases',
+        aggregationField: 'contentsubtypes',
+        params: [{
+          name: 'contentsubtype',
+          value: 'News Releases',
+        }],
+      }]}
+      RowComponent={ProjectContentListing}
+      tableColumns={[{
+        colSpan: 4,
+        colTitle: 'Title',
+      }, {
+        colSpan: 3,
+        colTitle: 'Expert',
+      }, {
+        colSpan: 2,
+        colTitle: 'Topic',
+      }, {
+        colSpan: 2,
+        colTitle: 'Type',
+      }, {
+        colSpan: 1,
+        colTitle: 'PDF',
+      }]}
+    />,
+    document.getElementById('project-search-table'),
+  );
+}
