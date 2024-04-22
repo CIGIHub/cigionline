c432e527bce10b8a2bf008335f66ee4242de3e60
yang-wang-ds <yangwang7890@gmail.com>
2024-04-22 13:39:02 -0400

    create essay page type if not exist

--- articles/migrations/0044_alter_article_series_article_page_type.py
+++ articles/migrations/0044_alter_article_series_article_page_type.py
@@ -4,6 +4,14 @@ from articles.models import ArticleSeriesPage, ArticleTypePage
 
 def update_article_pages(apps, schema_editor):
     series_pages = ArticleSeriesPage.objects.all()
+
+    if not ArticleTypePage.objects.filter(title='Essay').exists():
+        from wagtail.models import Page
+
+        parent_page = Page.objects.get(title='Articles').specific
+        new_page = ArticleTypePage(title='Essay')
+        parent_page.add_child(instance=new_page)
+
     essay_type = ArticleTypePage.objects.get(title='Essay')
 
     for series_page in series_pages:
