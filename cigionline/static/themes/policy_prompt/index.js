b52c8710f7070d876ccfe03ecc08c9bdde6a436e
johnbxu <johnbxu@gmail.com>
2024-05-01 14:56:39 -0300

    implement animation

--- cigionline/static/themes/policy_prompt/index.js
+++ cigionline/static/themes/policy_prompt/index.js
@@ -3,8 +3,6 @@ import 'mediaelement/src/css/mediaelementplayer.css';
 import 'mediaelement/src/css/mejs-controls.svg';
 import 'mediaelement/full';
 
-const podcastChapters = document.getElementById('podcast-chapters');
-
 $(document).ready(function () {
   // Add a class to the body to indicate that JS is enabled
   const podcastPlayer = $('#podcast-player').mediaelementplayer({
@@ -13,11 +11,43 @@ $(document).ready(function () {
     features: [
       'playpause',
       'current',
-      'controls',
       'progress',
       'duration',
+      'tracks',
       'volume',
     ],
+    success(media, node, player) {
+      const buildRewindButton = function () {
+        const button = document.createElement('button');
+        button.className = 'mejs__button mejs__rewind-button';
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-rotate-left';
+        button.appendChild(icon);
+        button.addEventListener('click', function () {
+          media.setCurrentTime(Math.max(media.getCurrentTime() - 15, 0));
+        });
+        return button;
+      };
+
+      const buildFastForwardButton = function () {
+        const button = document.createElement('button');
+        button.className = 'mejs__button mejs__fast-forward-button';
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-rotate-right';
+        button.appendChild(icon);
+        button.addEventListener('click', function () {
+          media.setCurrentTime(
+            Math.min(media.getCurrentTime() + 15, media.duration),
+          );
+        });
+        return button;
+      };
+
+      const controls = player.controls;
+      const playpause = controls.childNodes[0];
+      controls.insertBefore(buildRewindButton(), playpause);
+      controls.insertBefore(buildFastForwardButton(), playpause.nextSibling);
+    },
   });
 
   const svgUses = document.querySelectorAll('svg[class*="mejs__icon-"] > use');
@@ -33,11 +63,45 @@ $(document).ready(function () {
       const time = chapterTime.getAttribute('data-timestamp');
       // the time is in the format of hh:mm:ss. convert this time into seconds
       const timeArray = time.split(':');
-      const seconds = parseInt(timeArray[0], 10) * 3600
-        + parseInt(timeArray[1], 10) * 60
-        + parseInt(timeArray[2], 10);
+      const seconds =
+        parseInt(timeArray[0], 10) * 3600 +
+        parseInt(timeArray[1], 10) * 60 +
+        parseInt(timeArray[2], 10);
       podcastPlayer[0].setCurrentTime(seconds);
       podcastPlayer[0].play();
     });
   });
 });
+
+const text = ['prompt', 'policy'];
+const speed = 125;
+const textElement1 = document.getElementById('logo-animation-text-1');
+const textElement2 = document.getElementById('logo-animation-text-2');
+
+function typeText(word, element, index, callback) {
+  if (index < word.length) {
+    element.textContent += word[index];
+    setTimeout(() => {
+      typeText(word, element, index + 1, callback);
+    }, speed);
+  } else if (callback) {
+    callback();
+  }
+}
+
+function startTyping() {
+  typeText(text[0], textElement1, 0, () => {
+    textElement2.textContent = textElement1.textContent;
+    textElement1.textContent = '';
+    typeText(text[1], textElement1, 0, null);
+  });
+
+  setTimeout(() => {
+    document.getElementById('logo-animation-cursor').classList.add('wobble');
+  }, 2000);
+}
+
+document.getElementById('test-animation').addEventListener('click', startTyping);
+// setTimeout(() => {
+//   startTyping();
+// }, 2000);
