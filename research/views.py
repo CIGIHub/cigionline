601407d9f2f6a1c15da721aaeb5a1f2e6ba6d3b5
yang-wang-ds <yangwang7890@gmail.com>
2024-05-06 15:19:49 -0400

    topics and content pages

--- research/views.py
+++ research/views.py
@@ -2,6 +2,10 @@ from core.models import ArchiveablePageAbstract
 from django.http import JsonResponse
 
 from .models import TopicPage
+from articles.models import ArticlePage
+from publications.models import PublicationPage
+from multimedia.models import MultimediaPage
+from events.models import EventPage
 
 
 def all_topics(request):
@@ -16,3 +20,37 @@ def all_topics(request):
             'url': topic.get_url(request),
         } for topic in topics[:100]],
     }, safe=False)
+
+
+def topic_items(request):
+    topics = TopicPage.objects.live().filter(archive=0).order_by('title')
+    results = []
+
+    for topic in topics:
+        results.append({
+            "name": topic.title,
+            "children": [
+                {
+                    "name": "Articles",
+                    "value": ArticlePage.objects.filter(topics__in=[topic.id]).count(),
+                },
+                {
+                    "name": "Publications",
+                    "value": PublicationPage.objects.filter(topics__in=[topic.id]).count(),
+                },
+                {
+                    "name": "Multimedia",
+                    "value": MultimediaPage.objects.filter(topics__in=[topic.id]).count(),
+                },
+                {
+                    "name": "Events",
+                    "value": EventPage.objects.filter(topics__in=[topic.id]).count(),
+                },
+            ],
+            "content_pages": topic.content_pages.count(),
+        })
+
+    return JsonResponse({
+        "name": "Topics",
+        "children": results,
+    })
