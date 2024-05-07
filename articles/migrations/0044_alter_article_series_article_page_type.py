41f78f249dea397573479edf6cfcfd42b4e29e9e
johnbxu <johnbxu@gmail.com>
2024-05-07 10:40:44 -0300

    Fix migration

--- articles/migrations/0044_alter_article_series_article_page_type.py
+++ articles/migrations/0044_alter_article_series_article_page_type.py
@@ -1,33 +1,27 @@
 from django.db import migrations
 from articles.models import ArticleSeriesPage, ArticleTypePage
+from wagtail.models import Page
 
 
 def update_article_pages(apps, schema_editor):
     series_pages = ArticleSeriesPage.objects.all()
 
-    if not ArticleTypePage.objects.filter(title='Essay').exists():
-        from wagtail.models import Page
-        from articles.models import ArticleListPage
-
-        try:
-            parent_page = Page.objects.get(title='Articles').specific
-        except Page.DoesNotExist:
-            home = Page.objects.get(slug='Home')
-            parent_page = ArticleListPage(title='Articles')
-            home.add_child(instance=parent_page)
-            parent_page.save_revision().publish()
-
+    try:
+        parent_page = Page.objects.get(title='Articles').specific
         new_page = ArticleTypePage(title='Essay')
         parent_page.add_child(instance=new_page)
 
-    essay_type = ArticleTypePage.objects.get(title='Essay')
+        essay_type = ArticleTypePage.objects.get(title='Essay')
 
-    for series_page in series_pages:
-        for series_item in series_page.series_items.all():
-            page = series_item.content_page.specific
-            if page.__class__.__name__ == 'ArticlePage':
-                page.article_type = essay_type
-                page.save()
+        for series_page in series_pages:
+            for series_item in series_page.series_items.all():
+                page = series_item.content_page.specific
+                if page.__class__.__name__ == 'ArticlePage':
+                    page.article_type = essay_type
+                    page.save()
+
+    except Page.DoesNotExist:
+        pass
 
 
 class Migration(migrations.Migration):
