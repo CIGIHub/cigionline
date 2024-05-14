eac65d25f716616088a9f55f46fa5b15b8f82c02
yang-wang-ds <yangwang7890@gmail.com>
2024-05-14 11:53:17 -0400

    add print statement for migration with long execution time

--- research/migrations/0039_alter_topic_tags.py
+++ research/migrations/0039_alter_topic_tags.py
@@ -14,8 +14,10 @@ def many_to_one(apps, schema_editor):
     }
 
     for new_topic_title, old_topic_titles in many_to_one_migrations.items():
+        print(f'Updating {new_topic_title} from {old_topic_titles}')
         if TopicPage.objects.filter(title=new_topic_title).exists():
             new_topic = TopicPage.objects.get(title=new_topic_title)
+
         old_topics = [
             TopicPage.objects.get(title=old_topic_title)
             for old_topic_title in old_topic_titles if TopicPage.objects.filter(title=old_topic_title).exists()
@@ -24,17 +26,18 @@ def many_to_one(apps, schema_editor):
         # convert to set to dedup
         content_pages = list(set(ContentPage.objects.filter(topics__in=old_topics)))
 
-        for page in content_pages:
-            for old_topic in old_topics:
-                page.topics.remove(old_topic)
-            if new_topic not in page.topics.all():
-                page.topics.add(new_topic)
-            page.save()
+        if new_topic and old_topics:
+            for page in content_pages:
+                for old_topic in old_topics:
+                    page.topics.remove(old_topic)
+                if new_topic not in page.topics.all():
+                    page.topics.add(new_topic)
+                page.save()
 
-    for old_topic in old_topics:
-        if old_topic.archive == TopicPage.ArchiveStatus.UNARCHIVED:
-            old_topic.archive = TopicPage.ArchiveStatus.ARCHIVED
-            old_topic.save()
+            for old_topic in old_topics:
+                if old_topic.archive == TopicPage.ArchiveStatus.UNARCHIVED:
+                    old_topic.archive = TopicPage.ArchiveStatus.ARCHIVED
+                    old_topic.save()
 
 
 def one_to_many(apps, schema_editor):
