22ac067af87658cc42dd31e3404d66ace2bf43ce
yang-wang-ds <yangwang7890@gmail.com>
2024-05-22 10:25:47 -0400

    create article page by adding to parent page

--- research/tests.py
+++ research/tests.py
@@ -143,7 +143,7 @@ class HighlightedTopicsTests(WagtailPageTestCase):
 
     def _tag_topic(self, topic_title, article_title):
         def create_page(page_model, page_title, parent_page_title):
-            print('Creating page: {0}'.format(page_title))
+            print('Creating page: {0} under {1}'.format(page_title, parent_page_title))
             if not page_model.objects.filter(title=page_title).exists():
                 if Page.objects.filter(title=parent_page_title).exists():
                     new_page = page_model(title=page_title)
@@ -156,20 +156,24 @@ class HighlightedTopicsTests(WagtailPageTestCase):
                 print('Page already exists: {0}'.format(page_title))
 
         create_page(Page, 'Articles', 'Home')
-        create_page(ArticleTypePage, 'Test', 'Articles')
+        # create_page(ArticleTypePage, 'Test', 'Articles')
 
         if TopicPage.objects.filter(title=topic_title).exists():
             print('Topic exists: {0}'.format(topic_title))
 
-        ArticlePage.objects.create(
-            path='articles/{0}'.format(slugify(article_title)),
-            depth=2,
-            title=article_title,
-            slug=slugify(article_title),
-            publishing_date=date.today().strftime("%Y-%m-%d"),
-            article_type=ArticleTypePage.objects.get(title='Test'),
-            topics=[TopicPage.objects.get(title=topic_title)],
-            live=True)
+        # ArticlePage.objects.create(
+        #     path='articles/{0}'.format(slugify(article_title)),
+        #     depth=2,
+        #     title=article_title,
+        #     slug=slugify(article_title),
+        #     publishing_date=date.today().strftime("%Y-%m-%d"),
+        #     article_type=ArticleTypePage.objects.get(title='Test'),
+        #     topics=[TopicPage.objects.get(title=topic_title)],
+        #     live=True)
+        create_page(ArticlePage, article_title, 'Articles')
+        article_page = ArticlePage.objects.get(title=article_title)
+        article_page.topics.add(TopicPage.objects.get(title=topic_title))
+        article_page.save()
 
     def test_if_no_topics_template_should_be_empty(self):
         rendered = self.TEMPLATE.render(Context({}))
