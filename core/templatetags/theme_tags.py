3e738a80f01ab1fdc499fd2e8792448112140e04
johnbxu <johnbxu@gmail.com>
2024-07-15 13:12:05 -0300

    Add automatic term label to dph working papers page

---
+++ core/templatetags/theme_tags.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def dph_term(value):
+    date = datetime.strptime(value, '%Y-%m-%d %H:%M:%S%z')
+
+    year = date.year
+    month = date.month
+
+    if (month <= 4):
+        term = 'Fall'
+    elif 5 <= month <= 8:
+        term = 'Winter'
+    elif 9 <= month <= 12:
+        term = 'Summer'
+    else:
+        term = ''
+
+    return f'{term} {year} term'
