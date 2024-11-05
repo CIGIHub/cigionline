b315b92f6c05ea6f438ee7c100d16402bceaf78b
Jonathan Xu <johnbxu@gmail.com>
2024-11-05 13:03:18 -0400

    Add abstract page styling

---
+++ cigionline/static/pages/think7/abstract_submission_page/index.js
@@ -0,0 +1,38 @@
+import './css/_abstract_submission_page.scss';
+
+document
+  .getElementById('abstractUploadForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('/abstract-submission/', {
+      method: 'POST',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const responseMessage = document.getElementById('responseMessage');
+
+        if (data.status === 'success') {
+          responseMessage.innerText = data.message;
+          responseMessage.style.color = 'green';
+        } else {
+          responseMessage.innerText = data.message;
+          responseMessage.style.color = 'red';
+        }
+
+        responseMessage.style.display = 'block';
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        const responseMessage = document.getElementById('responseMessage');
+        responseMessage.innerText = 'An unexpected error occurred.';
+        responseMessage.style.color = 'red';
+        responseMessage.style.display = 'block';
+      });
+  });
