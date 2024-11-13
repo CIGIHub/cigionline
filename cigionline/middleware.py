f3f864f4b55d435a91c024ed8d63143e0c985549
Jonathan Xu <johnbxu@gmail.com>
2024-11-13 14:07:20 -0400

    CIGI-1039: Add basic auth middleware for think7 domain (#2887)
    * Add basic auth middleware for think7 domain
    
    * Update middleware

---
+++ cigionline/middleware.py
@@ -0,0 +1,42 @@
+import base64, os
+from django.http import HttpResponse
+
+
+class BasicAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        target_domain = "think7canada.org"
+
+        if request.get_host() == target_domain:
+            auth = request.META.get("HTTP_AUTHORIZATION")
+
+            if (
+                "BASIC_AUTH_USER_THINK7" in os.environ
+                and "BASIC_AUTH_PASSWORD_THINK7" in os.environ
+            ):
+                BASIC_AUTH_USER_THINK7 = os.environ.get("BASIC_AUTH_USER_THINK7")
+                BASIC_AUTH_PASSWORD_THINK7 = os.environ.get(
+                    "BASIC_AUTH_PASSWORD_THINK7"
+                )
+
+                if auth:
+                    auth_type, encoded_credentials = auth.split(" ", 1)
+                    if auth_type.lower() == "basic":
+                        credentials = base64.b64decode(encoded_credentials).decode(
+                            "utf-8"
+                        )
+                        username, password = credentials.split(":", 1)
+
+                        if (
+                            username == BASIC_AUTH_USER_THINK7
+                            and password == BASIC_AUTH_PASSWORD_THINK7
+                        ):
+                            return self.get_response(request)
+
+                response = HttpResponse("Unauthorized", status=401)
+                response["WWW-Authenticate"] = 'Basic realm="Protected"'
+                return response
+
+        return self.get_response(request)
