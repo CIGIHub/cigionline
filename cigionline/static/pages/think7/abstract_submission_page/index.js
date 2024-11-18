c2ae59b4ffc5873cce62fddb2ce875370a5fdcdb
Jonathan Xu <johnbxu@gmail.com>
2024-11-18 12:03:00 -0400

    linter

--- cigionline/static/pages/think7/abstract_submission_page/index.js
+++ cigionline/static/pages/think7/abstract_submission_page/index.js
@@ -1,41 +1,42 @@
 import './css/_abstract_submission_page.scss';
 
 const abstractUploadForm = document.getElementById('abstractUploadForm');
-abstractUploadForm
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
+abstractUploadForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    const formData = new FormData(this);
-    const responseMessage = document.getElementById('responseMessage');
-    responseMessage.classList.add('loading');
-    responseMessage.innerText = 'Uploading file...';
+  const formData = new FormData(this);
+  const responseMessage = document.getElementById('responseMessage');
+  responseMessage.classList.remove('success');
+  responseMessage.classList.remove('error');
+  responseMessage.classList.add('loading');
+  responseMessage.innerText = 'Uploading file...';
 
-    fetch('/abstract-submission/', {
-      method: 'POST',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status === 'success') {
-          responseMessage.innerText = data.message;
-          responseMessage.classList.remove('loading');
-          responseMessage.classList.remove('error');
-          responseMessage.classList.add('success');
-          abstractUploadForm.querySelector('input[type="file"]').value = '';
-        } else {
-          responseMessage.innerText = data.message;
-          responseMessage.classList.remove('loading');
-          responseMessage.classList.remove('success');
-          responseMessage.classList.add('error');
-        }
-      })
-      .catch(() => {
-        responseMessage.innerText = 'An unexpected error occurred.';
+  fetch('/abstract-submission/', {
+    method: 'POST',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === 'success') {
+        responseMessage.innerText = data.message;
+        responseMessage.classList.remove('loading');
+        responseMessage.classList.remove('error');
+        responseMessage.classList.add('success');
+        abstractUploadForm.querySelector('input[type="file"]').value = '';
+      } else {
+        responseMessage.innerText = data.message;
         responseMessage.classList.remove('loading');
         responseMessage.classList.remove('success');
         responseMessage.classList.add('error');
-      });
-  });
+      }
+    })
+    .catch(() => {
+      responseMessage.innerText = 'An unexpected error occurred.';
+      responseMessage.classList.remove('loading');
+      responseMessage.classList.remove('success');
+      responseMessage.classList.add('error');
+    });
+});
