3679f8485753925d932e60d94c20c4c9703258d1
Jonathan Xu <johnbxu@gmail.com>
2024-11-21 12:02:39 -0400

    Add GA tracking code

--- cigionline/static/pages/think7/index.js
+++ cigionline/static/pages/think7/index.js
@@ -1,15 +1,31 @@
 import './css/think7.scss';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import CookieConsent from '../../js/components/CookieConsent';
 
 $(function () {
-  document.getElementById('open-menu-btn').addEventListener('click', function (e) {
-    e.stopPropagation();
-    this.classList.toggle('open');
+  document
+    .getElementById('open-menu-btn')
+    .addEventListener('click', function (e) {
+      e.stopPropagation();
+      this.classList.toggle('open');
 
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.classList.toggle('show');
+      const dropdownMenu = document.getElementById('dropdown-menu');
+      dropdownMenu.classList.toggle('show');
 
-    // Optionally update the aria-expanded attribute for accessibility
-    const isExpanded = this.getAttribute('aria-expanded') === 'true';
-    this.setAttribute('aria-expanded', !isExpanded);
-  });
+      const isExpanded = this.getAttribute('aria-expanded') === 'true';
+      this.setAttribute('aria-expanded', !isExpanded);
+    });
 });
+
+// const cookieConsentContainer = document.getElementById(
+//   'cigi-cookie-consent-container',
+// );
+// if (
+//   cookieConsentContainer &&
+//   !document.cookie
+//     .split(';')
+//     .some((item) => item.includes('cigionline.accept.privacy.notice=1'))
+// ) {
+//   ReactDOM.render(<CookieConsent />, cookieConsentContainer);
+// }
