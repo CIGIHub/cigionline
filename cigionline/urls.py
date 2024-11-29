5ea6ed55f304df4b1771f9b0030a355984aae4b1
yang-wang-ds <50630672+yang-wang-ds@users.noreply.github.com>
2024-11-28 20:16:36 -0500

    CIGI-1041: Newsletter subscribe form on Think7 Footer (#2894)
    * add subscribe box in foot; style
    
    * remove subscribe landing scss; fix setting env vars
    
    * update email css
    
    * remove comments; reorganize header footer inclusion
    
    * mobile breakpoint

--- cigionline/urls.py
+++ cigionline/urls.py
@@ -12,7 +12,7 @@ from robots import views as robots_views
 from search import views as search_views
 from events import views as events_views
 from articles import views as article_views
-from subscribe.views import subscribe_dph
+from subscribe.views import subscribe_dph, subscribe_think7
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.contrib.sitemaps.views import sitemap
@@ -46,6 +46,7 @@ urlpatterns = urlpatterns + [
     re_path(r'^api/article_series_article_pages/$', article_views.get_article_series_article_pages),
 
     re_path(r'^subscribe_dph$', subscribe_dph, name='subscribe_dph'),
+    re_path(r'^subscribe$', subscribe_think7, name='subscribe_think7'),
     re_path(r'^events/feed.ics$', EventFeed()),
     re_path(r'^favicon\.ico$', favicon_view),
     re_path(r'^robots\.txt$', robots_views.RobotsView.as_view(), name='robots'),
