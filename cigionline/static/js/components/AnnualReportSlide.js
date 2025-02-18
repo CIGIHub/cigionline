065c3dfbfeb4ce4a654f43dc58d3c2450382cb88
Jonathan Xu <johnbxu@gmail.com>
2025-02-18 16:31:28 -0400

    Add slide list component

--- cigionline/static/js/components/AnnualReportSlide.js
+++ cigionline/static/js/components/AnnualReportSlide.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import '../../css/components/AnnualReportSlide.scss';
 import AnnualReportNav from './AnnualReportNav';
+import AnnualReportSlideList from './AnnualReportSlideList';
 
 const AnnualReportSlide = ({ slides, basePath }) => {
   const { slug } = useParams();
@@ -24,30 +25,51 @@ const AnnualReportSlide = ({ slides, basePath }) => {
     currentIndex < slides.length - 1 ? slides[currentIndex + 1] : null;
 
   useEffect(() => {
-    const handleScroll = (event) => {
-      if (isScrolling) return; // Prevent rapid scrolling
+    const handleNavigation = (direction) => {
+      if (isScrolling) return; // Prevent rapid navigation
       setIsScrolling(true);
+      setTimeout(() => setIsScrolling(false), 1000); // Cooldown
 
-      setTimeout(() => setIsScrolling(false), 1000); // Cooldown period
-
-      if (event.deltaY > 0 && nextSlide) {
+      if (direction === 'next' && nextSlide) {
         navigate(`${basePath}/${nextSlide.slug}`);
-      } else if (event.deltaY < 0 && prevSlide) {
+      } else if (direction === 'prev' && prevSlide) {
         navigate(`${basePath}/${prevSlide.slug}`);
       }
     };
 
+    const handleScroll = (event) => {
+      if (event.deltaY > 0) {
+        handleNavigation('next');
+      } else if (event.deltaY < 0) {
+        handleNavigation('prev');
+      }
+    };
+
+    const handleKeyDown = (event) => {
+      if (['ArrowDown', 'PageDown'].includes(event.key)) {
+        handleNavigation('next');
+      } else if (['ArrowUp', 'PageUp'].includes(event.key)) {
+        handleNavigation('prev');
+      }
+    };
+
     window.addEventListener('wheel', handleScroll);
-    return () => window.removeEventListener('wheel', handleScroll);
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+      window.removeEventListener('keydown', handleKeyDown);
+    };
   }, [currentIndex, navigate, isScrolling]);
 
   return (
     <div className="annual-report-slide">
       <div>{slides[currentIndex].id}</div>
-      <h1>{slides[currentIndex].slide_title}</h1>
+      <h1 aria-live="assertive">{slides[currentIndex].slide_title}</h1>
       <div
         dangerouslySetInnerHTML={{ __html: slides[currentIndex].slide_content }}
       />
+      <AnnualReportSlideList slides={slides} basePath={basePath} />
       <AnnualReportNav
         basePath={basePath}
         prevSlide={prevSlide}
