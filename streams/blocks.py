357e91bafc2c1a7a107e8268d8346296521bd6a6
Jonathan Xu <johnbxu@gmail.com>
2025-05-29 13:52:52 -0400

    CIGI-1072: Implement Digital Finance event theme (#2927)
    * Add event page submission form
    
    * add template and theme files
    
    * Add abstract submission section and update styling
    
    * Add footer styling
    
    * style footer
    
    * Add banner background
    
    * merge migrations
    
    * Style footer
    
    * Update event info and take out subscribe section
    
    * remove subscribe section
    
    * Implement collapsible paragraphs
    
    * Updated collection info
    
    * Update collapsible para block and fix top bar issue
    
    * Spacing updates
    
    * Update email settings
    
    * Update footer email address
    
    * Update confirmation email subject
    
    * Add subscription block back
    
    * full height background image
    
    * Adjust footer spacing
    
    * Adjust title
    
    * linter

--- streams/blocks.py
+++ streams/blocks.py
@@ -1576,3 +1576,67 @@ class T7CommuniqueBlock(blocks.StructBlock):
         icon = 'doc-full'
         label = 'T7 Communique'
         template = 'streams/t7_communique_block.html'
+
+
+class AbstractSubmissionBlock(blocks.StructBlock):
+    # Renders a block that contains a form for submitting abstracts.
+    class Meta:
+        icon = 'form'
+        label = 'Abstract Submission Form'
+        template = 'streams/abstract_submission_form_block.html'
+
+
+class CollapsibleParagraphBlock(blocks.StructBlock):
+    """Standard text paragraph that is collapsible."""
+
+    collapsed = blocks.BooleanBlock(
+        required=False,
+        default=True,
+        help_text="If checked, the paragraph will be collapsed by default.",
+    )
+    title = blocks.RichTextBlock(
+        required=False,
+        features=[
+            'bold',
+            'italic',
+            'link',
+        ],
+        help_text="Optional title for the collapsible paragraph.",
+    )
+    title_type = blocks.ChoiceBlock(
+        choices=[
+            ('h2', 'H2'),
+            ('h3', 'H3'),
+            ('h4', 'H4'),
+        ],
+        default='h2',
+        help_text="Select the heading type for the title.",
+    )
+    paragraph = blocks.RichTextBlock(
+        required=True,
+        features=[
+            'bold',
+            'dropcap',
+            'endofarticle',
+            'paragraph_heading',
+            'h2',
+            'h3',
+            'h4',
+            'hr',
+            'image',
+            'italic',
+            'underline',
+            'link',
+            'ol',
+            'subscript',
+            'superscript',
+            'ul',
+            'anchor',
+            'rtl',
+        ],
+    )
+
+    class Meta:
+        icon = 'edit'
+        label = 'Collapsible Paragraph'
+        template = 'streams/collapsible_paragraph_block.html'
