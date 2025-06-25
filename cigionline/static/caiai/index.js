753b92cc6b8a8682daf0b8478e532202adfb4d10
Jonathan Xu <johnbxu@gmail.com>
2025-06-25 08:14:02 -0300

    Add bio block

--- cigionline/static/caiai/index.js
+++ cigionline/static/caiai/index.js
@@ -1,1 +1,21 @@
 import './css/caiai.scss';
+
+document.querySelectorAll('.open-modal-btn').forEach((button) => {
+  button.addEventListener('click', () => {
+    const modalId = button.getAttribute('data-modal');
+    document.getElementById(modalId).classList.add('show');
+  });
+});
+
+document.querySelectorAll('.close-btn').forEach((button) => {
+  button.addEventListener('click', () => {
+    const modalId = button.getAttribute('data-modal');
+    document.getElementById(modalId).classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal')) {
+    e.target.classList.remove('show');
+  }
+});
