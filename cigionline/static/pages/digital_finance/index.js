02631a02302773281a5f602294d7781effd16703
Jonathan Xu <johnbxu@gmail.com>
2025-09-22 16:31:27 -0300

    Update text transformation for h2 elements and comment out event upload form logic

--- cigionline/static/pages/digital_finance/index.js
+++ cigionline/static/pages/digital_finance/index.js
@@ -22,45 +22,45 @@ if (collapsibleParagraphs) {
     });
   });
 }
-const eventUploadForm = document.getElementById('eventUploadForm');
-eventUploadForm.addEventListener('submit', function (event) {
-  event.preventDefault();
+// const eventUploadForm = document.getElementById('eventUploadForm');
+// eventUploadForm.addEventListener('submit', function (event) {
+//   event.preventDefault();
 
-  const formData = new FormData(this);
-  const responseMessage = document.getElementById('responseMessage');
-  responseMessage.classList.remove('success');
-  responseMessage.classList.remove('error');
-  responseMessage.classList.add('loading');
-  responseMessage.innerText = 'Uploading file...';
+//   const formData = new FormData(this);
+//   const responseMessage = document.getElementById('responseMessage');
+//   responseMessage.classList.remove('success');
+//   responseMessage.classList.remove('error');
+//   responseMessage.classList.add('loading');
+//   responseMessage.innerText = 'Uploading file...';
 
-  fetch(window.location.pathname, {
-    method: 'POST',
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRFToken': CSRFToken,
-    },
-    body: formData,
-    credentials: 'same-origin',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.status === 'success') {
-        responseMessage.innerText = data.message;
-        responseMessage.classList.remove('loading');
-        responseMessage.classList.remove('error');
-        responseMessage.classList.add('success');
-        eventUploadForm.querySelector('input[type="file"]').value = '';
-      } else {
-        responseMessage.innerText = data.message;
-        responseMessage.classList.remove('loading');
-        responseMessage.classList.remove('success');
-        responseMessage.classList.add('error');
-      }
-    })
-    .catch(() => {
-      responseMessage.innerText = 'An unexpected error occurred.';
-      responseMessage.classList.remove('loading');
-      responseMessage.classList.remove('success');
-      responseMessage.classList.add('error');
-    });
-});
+//   fetch(window.location.pathname, {
+//     method: 'POST',
+//     headers: {
+//       'X-Requested-With': 'XMLHttpRequest',
+//       'X-CSRFToken': CSRFToken,
+//     },
+//     body: formData,
+//     credentials: 'same-origin',
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.status === 'success') {
+//         responseMessage.innerText = data.message;
+//         responseMessage.classList.remove('loading');
+//         responseMessage.classList.remove('error');
+//         responseMessage.classList.add('success');
+//         eventUploadForm.querySelector('input[type="file"]').value = '';
+//       } else {
+//         responseMessage.innerText = data.message;
+//         responseMessage.classList.remove('loading');
+//         responseMessage.classList.remove('success');
+//         responseMessage.classList.add('error');
+//       }
+//     })
+//     .catch(() => {
+//       responseMessage.innerText = 'An unexpected error occurred.';
+//       responseMessage.classList.remove('loading');
+//       responseMessage.classList.remove('success');
+//       responseMessage.classList.add('error');
+//     });
+// });
