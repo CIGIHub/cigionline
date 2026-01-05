39e852909f55d09febafc6431800a714cd9e601e
Jonathan Xu <johnbxu@gmail.com>
2026-01-05 17:00:26 -0400

    Update annualreportlisting

--- cigionline/static/js/components/AnnualReportListing.js
+++ cigionline/static/js/components/AnnualReportListing.js
@@ -4,27 +4,29 @@ import React from 'react';
 function AnnualReportListing(props) {
   const { row } = props;
   const { handleCTAClick } = props;
+  const reportFrLink = row.year >= 2025
+    ? `https://www.cigionline.org/about/annual-report/${row.year}/interactive/fr/home/`
+    : `https://www.cigionline.org${row.report_interactive}fr`;
 
   return (
     <tr>
       <td colSpan="4">
-        <div className="table-mobile-text">
-          Year
-        </div>
+        <div className="table-mobile-text">Year</div>
         <div className="table-content">
-          <a href={row.url}>
-            {`${row.year} Annual Report`}
-          </a>
+          <a href={row.url}>{`${row.year} Annual Report`}</a>
         </div>
       </td>
       <td colSpan="2">
         {row.report_interactive && (
           <>
-            <div className="table-mobile-text">
-              Digital Interactive
-            </div>
+            <div className="table-mobile-text">Digital Interactive</div>
             <div className="table-content">
-              <a className="button-action track-cta" data-cta="ar-interactive" onClick={handleCTAClick} href={`https://www.cigionline.org${row.report_interactive}`}>
+              <a
+                className="button-action track-cta"
+                data-cta="ar-interactive"
+                onClick={handleCTAClick}
+                href={`https://www.cigionline.org${row.report_interactive}`}
+              >
                 <i className="fas fa-mouse-pointer" />
                 Explore
               </a>
@@ -33,9 +35,7 @@ function AnnualReportListing(props) {
         )}
       </td>
       <td colSpan="2">
-        <div className="table-mobile-text">
-          English
-        </div>
+        <div className="table-mobile-text">English</div>
         <div className="table-content">
           {(row.report_english && (
             <a
@@ -52,7 +52,7 @@ function AnnualReportListing(props) {
               className="table-btn-icon"
               data-cta="ar-interactive"
               onClick={handleCTAClick}
-              href={`https://www.cigionline.org${row.report_interactive}en`}
+              href={`https://www.cigionline.org${row.report_interactive}`}
               aria-label="Explore English Interactive Annual Report"
             >
               <i className="fas fa-mouse-pointer" />
@@ -61,9 +61,7 @@ function AnnualReportListing(props) {
         </div>
       </td>
       <td colSpan="2">
-        <div className="table-mobile-text">
-          en français
-        </div>
+        <div className="table-mobile-text">en français</div>
         <div className="table-content">
           {(row.report_french && (
             <a
@@ -80,7 +78,7 @@ function AnnualReportListing(props) {
               className="table-btn-icon track-cta"
               data-cta="ar-interactive"
               onClick={handleCTAClick}
-              href={`https://www.cigionline.org${row.report_interactive}fr`}
+              href={reportFrLink}
               aria-label="Explore French Interactive Annual Report"
             >
               <i className="fas fa-mouse-pointer" />
@@ -91,9 +89,7 @@ function AnnualReportListing(props) {
       <td colSpan="2">
         {row.report_financial && (
           <>
-            <div className="table-mobile-text">
-              Financial Statement
-            </div>
+            <div className="table-mobile-text">Financial Statement</div>
             <div className="table-content">
               <a
                 className="table-btn-icon track-cta"
