19523473427a307ff64858c91a04e642f6e04e55
Jonathan Xu <johnbxu@gmail.com>
2026-02-26 16:08:32 -0400

    Try fix for image issue

--- images/models.py
+++ images/models.py
@@ -1,3 +1,4 @@
+from django.core.files.base import ContentFile
 from django.db import models
 
 from wagtail.images.models import Image, AbstractImage, AbstractRendition
@@ -9,6 +10,22 @@ class CigionlineImage(AbstractImage):
     admin_form_fields = Image.admin_form_fields + (
         'caption',
     )
+
+    def generate_rendition_file(self, filter, *, source=None):
+        """
+        Override to buffer the generated SpooledTemporaryFile into an
+        in-memory ContentFile before returning.
+
+        Django 5.2 introduced ImageFieldFile._set_instance_attribute which
+        triggers update_dimension_fields(force=True) immediately after S3
+        uploads the file. S3 (django-storages) closes the underlying
+        SpooledTemporaryFile during upload, causing Django to fail when it
+        tries to re-read the file for dimensions. A ContentFile (BytesIO)
+        is not closed by S3, so dimensions can still be read afterward.
+        """
+        generated_file = super().generate_rendition_file(filter, source=source)
+        generated_file.seek(0)
+        return ContentFile(generated_file.read(), name=generated_file.name)
 
 
 class CigionlineRendition(AbstractRendition):
