a5d24b4347f1be043cb967d29fcd55d9587ebaac
Jonathan Xu <johnbxu@gmail.com>
2026-03-31 10:38:51 -0300

    Implement custom issuu embed

--- embeds/finders/oembed.py
+++ embeds/finders/oembed.py
@@ -1,9 +1,52 @@
+import re
+
 from bs4 import BeautifulSoup
 from django.core.exceptions import ImproperlyConfigured
 from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
+from wagtail.embeds.exceptions import EmbedNotFoundException
+from wagtail.embeds.finders.base import EmbedFinder
 from wagtail.embeds.finders.oembed import OEmbedFinder
 from wagtail.embeds.oembed_providers import youtube
 
+ISSUU_URL_PATTERN = re.compile(r'^https?://(?:www\.)?issuu\.com/([^#?/]+)/docs/([^#?/]+)', re.IGNORECASE)
+
+
+class IssuuEmbedFinder(EmbedFinder):
+    def accept(self, url):
+        return bool(ISSUU_URL_PATTERN.match(url))
+
+    def find_embed(self, url, max_width=None, max_height=None):
+        match = ISSUU_URL_PATTERN.match(url)
+        if not match:
+            raise EmbedNotFoundException
+
+        username = match.group(1)
+        docname = match.group(2)
+        embed_url = 'https://e.issuu.com/embed.html?u={}&d={}'.format(username, docname)
+        width = max_width or 760
+        html = (
+            '<div style="position:relative;padding-top:max(60%,326px);height:0;width:100%">'
+            '<iframe'
+            ' allow="clipboard-write"'
+            ' sandbox="allow-top-navigation allow-top-navigation-by-user-activation'
+            ' allow-downloads allow-scripts allow-same-origin allow-popups'
+                ' allow-modals allow-popups-to-escape-sandbox allow-forms"'
+                ' allowfullscreen="true"'
+            ' style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;"'
+            ' src="{}">'
+            '</iframe></div>'
+        ).format(embed_url)
+
+        return {
+            'title': '',
+            'author_name': '',
+            'provider_name': 'Issuu',
+            'type': 'rich',
+            'thumbnail_url': None,
+            'width': width,
+            'height': None,
+            'html': html,
+        }
 
 class YouTubeOEmbedFinder(OEmbedFinder):
     def __init__(self, providers=None, options=None):
